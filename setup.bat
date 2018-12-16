f8b661c4d19594edd777c252f02158a9b6519173
daivieth <daiviet.huynh@gmail.com>
2018-12-16 11:07:32 +0700

    d - -12-2018 1107

--- setup.bat
+++ setup.bat
@@ -7,8 +7,15 @@ SET _PIP_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\Scripts\pip.exe"
 SET _ACTIVATE="%APP_DIR%\flask-prod\Scripts\activate.bat"
 SET _VIRTUALENV="%LOCALAPPDATA%\Programs\Python\%PY_VER%\Scripts\virtualenv.exe"
 
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set cdate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set ctime=%%a%%b)
+echo %mydate%_%mytime%
+
 %_PIP_EXE% install virtualenv
 cd %APP_DIR%
+git add .
+git commit -m "d - %cdate% %ctime%"
+
 %_VIRTUALENV% flask-prod
 
 @ECHO ###########################
