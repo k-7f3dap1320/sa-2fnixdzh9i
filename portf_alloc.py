66559b24e27ab36d5e0cf5db1599003541822278
daivieth <daiviet.huynh@gmail.com>
2018-12-18 16:21:13 +0700

    d-18/12/2018-16:21:12.26

--- portf_alloc.py
+++ portf_alloc.py
@@ -15,6 +15,37 @@ def get_portf_alloc(uid):
 
     try:
 
+        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT portfolios.order_type, portfolios.quantity, portfolios.symbol, portfolios.entry_level, portfolios.expiration, "+\
+        "symbol_list.uid FROM portfolios JOIN symbol_list ON portfolios.portf_symbol = symbol_list.symbol "+\
+        "WHERE symbol_list.uid=" + str(uid) + " ORDER BY portfolios.symbol"
+        cr.execute(sql)
+        rs = cr.fetchall()
+        signal_box_data = ''
+        for row in rs:
+            order_type = row[0]
+            quantity = row[1]
+            symbol = row[2]
+            entry_price = row[3]
+            trade_expiration = row[4]
+            exp_date_str = trade_expiration.strftime("%d-%b-%Y")
+
+            if order_type == 'buy':
+                badge = 'badge-success'
+            else:
+                badge = 'badge-danger'
+            signal_box_data = signal_box_data + '' +\
+            '                       <tr>'+\
+            '                          <th scope="row"><span class="badge '+ badge +'">'+ order_type +'</span></th>'+\
+            '                          <td>'+ str(quantity)  +'</td>'+\
+            '                          <td>'+ symbol +'</td>'+\
+            '                          <td>'+ str(entry_price) +'</td>'+\
+            '                          <td>'+ exp_date_str +'</td>'+\
+            '                       </tr>'
+        cr.close()
+        connection.close()
+
         signal_box = '' +\
         '        <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">'+\
         '            <div class="box-part">'+\
@@ -29,70 +60,97 @@ def get_portf_alloc(uid):
         '                       </tr>'+\
         '                   </thead>'+\
         '                   <tbody>'
-
-        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-        cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT portfolios.order_type, portfolios.quantity, portfolios.symbol, portfolios.entry_level, portfolios.expiration, "+\
-        "symbol_list.uid FROM portfolios JOIN symbol_list ON portfolios.portf_symbol = symbol_list.symbol "+\
-        "WHERE symbol_list.uid=" + str(uid) + " ORDER BY portfolios.symbol"
-
-        print(sql)
-
-        cr.execute(sql)
-        rs = cr.fetchall()
-        for row in rs:
-            order_type = row[0]
-            quantity = row[1]
-            symbol = row[2]
-            entry_price = row[3]
-            trade_expiration = row[4]
-
-            if order_type == 'buy':
-                badge = 'badge-success'
-            else:
-                badge = 'badge-danger'
-
-            signal_box = signal_box + '' +\
-            '                       <tr>'+\
-            '                          <th scope="row"><span class="badge '+ badge +'">'+ order_type +'</span></th>'+\
-            '                          <td>'+ str(quantity)  +'</td>'+\
-            '                          <td>'+ symbol +'</td>'+\
-            '                          <td>'+ str(entry_price) +'</td>'+\
-            '                          <td>'+ str(trade_expiration) +'</td>'+\
-            '                       </tr>'
+        signal_box = signal_box + signal_box_data
         signal_box = signal_box + '' +\
         '                   </tbody>'+\
         '               </table>'+\
         '            </div>'+\
         '        </div>'
-        cr.close()
-        connection.close()
+
+        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT count(1) FROM `portfolios` JOIN symbol_list ON portfolios.portf_symbol = symbol_list.symbol WHERE symbol_list.uid=" + str(uid)
+        cr.execute(sql); rs = cr.fetchall()
+        for row in rs:
+            num_rec = row[0]
+        num_rec = round( 255 / num_rec, 0)
+
+        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT portfolios.alloc_fullname, portfolios.order_type, portfolios.dollar_amount "+\
+        "FROM `portfolios` JOIN symbol_list ON portfolios.portf_symbol = symbol_list.symbol WHERE symbol_list.uid=" + str(uid) + " "+\
+        "ORDER BY portfolios.dollar_amount"
+        cr.execute(sql)
+        rs = cr.fetchall()
+        pie_chart_data = ''
+        pie_chart_option = ''
+        i = 0
+        count_buy = 0
+        count_sell = 0
+        sell_color_R = 255; sell_color_G = 0; sell_color_B = 0
+        buy_color_R = 198; buy_color_G = 255; buy_color_B = 0
+
+        for row in rs:
+            alloc_fullname = row[0]
+            order_type = row[1]
+            dollar_amount = row[2]
+            if i == 0:
+                pie_chart_data = '["'+ alloc_fullname +'", '+ str(dollar_amount) +']'
+            else:
+                pie_chart_data = pie_chart_data + ', ' + '["'+ alloc_fullname +'", '+ str(dollar_amount) +']'
+            if order_type == 'buy':
+                if buy_color_B > num_rec:
+                    buy_color_R = buy_color_R - num_rec
+                if buy_color_G > num_rec:
+                    buy_color_G = buy_color_G - num_rec
+                if buy_color_B > num_rec:
+                    buy_color_B = buy_color_B - num_rec
+                if i == 0:
+                    pie_chart_option = '{'+ str(i) +':{color:"rgb('+ buy_color_R +','+ buy_color_G +','+ buy_color_B +')"}}'
+                else:
+                    pie_chart_option = pie_chart_option + ', {'+ str(i) +':{color:"rgb('+ buy_color_R +','+ buy_color_G +','+ buy_color_B +')"}}'
+            if order_type == 'sell':
+                if buy_color_B > num_rec:
+                    buy_color_R = buy_color_R - num_rec
+                if buy_color_G < (255 - num_rec):
+                    buy_color_G = buy_color_G + num_rec
+                if buy_color_B < (255 - num_rec):
+                    buy_color_B = buy_color_B + num_rec
+                if i == 0:
+                    pie_chart_option = '{'+ str(i) +':{color:"rgb('+ sell_color_R +','+ sell_color_G +','+ sell_color_B +')"}}'
+                else:
+                    pie_chart_option = pie_chart_option + ', {'+ str(i) +':{color:"rgb('+ sell_color_R +','+ sell_color_G +','+ sell_color_B +')"}}'
+
+            i += 1
 
         pie_chart_title = 'Portfolio Allocation'
+        pie_chart_x = "Allocation"
+        pie_chart_y = "Dollar Amount"
         pie_chart = '' +\
         '        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">'+\
-        '            <div class="box-part">'+\
-        '    <script type="text/javascript">'+\
-        '      google.charts.load("current", {packages:["corechart"]});'+\
-        '      google.charts.setOnLoadCallback(drawChart);'+\
-        '      function drawChart() {'+\
-        '        var data = google.visualization.arrayToDataTable(['+\
-        '          ["Language", "Speakers (in millions)"],'+\
-        '          ["Assamese", 13], ["Bengali", 83], ["Bodo", 1.4],'+\
-        '        ]);'+\
-        '        var options = {'+\
-        '          title: "'+ pie_chart_title +'",'+\
-        '          pieHole: 0.4,'+\
-        '          legend: "none",'+\
-        '          pieSliceText: "label",'+\
-        '          slices: {  2: {offset: 0.2},'+\
-        '          },'+\
-        '        };'+\
-        '        var chart = new google.visualization.PieChart(document.getElementById("piechart"));'+\
-        '        chart.draw(data, options);'+\
-        '      }'+\
-        '    </script>'+\
-        '            </div>'+\
+        '           <div class="box-part">'+\
+        '               <script type="text/javascript">'+\
+        '                   google.charts.load("current", {packages:["corechart"]});'+\
+        '                   google.charts.setOnLoadCallback(drawChart);'+\
+        '                   function drawChart() {'+\
+        '                       var data = google.visualization.arrayToDataTable(['+\
+        '                       ["'+ pie_chart_x +'", "'+ pie_chart_y +'"],'
+        pie_chart = pie_chart + pie_chart_data
+        pie_chart = pie_chart + '' +\
+        '                       ]);'+\
+        '                       var options = {'+\
+        '                           title: "'+ pie_chart_title +'",'+\
+        '                           pieHole: 0.2,'+\
+        '                           legend: "none",'+\
+        '                           pieSliceText: "percentage",'
+        '                           slices: ' + pie_chart_option  + ''+\
+        '                           legend: {position:"labeled", textStyle: {fontSize: 11}}'+\
+        '                       },'+\
+        '                       var chart = new google.visualization.PieChart(document.getElementById("piechart"));'+\
+        '                       chart.draw(data, options);'+\
+        '                   }'+\
+        '               </script>'+\
+        '           </div>'+\
         '        </div>'
 
 
