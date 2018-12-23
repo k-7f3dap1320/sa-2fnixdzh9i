e422b8a1a77987445a8fd0a319b0861282658c8f
daivieth <daiviet.huynh@gmail.com>
2018-12-23 20:22:49 +0700

    dev

--- signal_ta_chart_alt_orders.py
+++ signal_ta_chart_alt_orders.py
@@ -171,10 +171,10 @@ def get_ta_chart(uid):
     '          vAxis: { textStyle: {fontSize:'+ str(chart_font_size) +'}, gridlines: { color: "transparent" } },'+\
     '          hAxis: {textStyle: {fontSize:'+ str(chart_font_size) +'}, gridlines: { count: 4 } }, '+\
     '          series:{'+\
-    '                   0: {areaOpacity: 0.1, color: "#9fc1e1", lineWidth: 2},'+\
-    '                   1: {areaOpacity: 0.1, color: "#ff9800", lineWidth: 1},'+\
-    '                   2: {areaOpacity: 0.1, color: "black", lineWidth: 1},'+\
-    '                   3: {areaOpacity: 0, color: "black", lineWidth: 1},'+\
+    '                   0: {areaOpacity: 0.1, color: "#3f84c5", lineWidth: 2},'+\
+    '                   1: {areaOpacity: 0.1, color: "#ff9800", lineWidth: 2},'+\
+    '                   2: {areaOpacity: 0.1, color: "#47808d", lineWidth: 1},'+\
+    '                   3: {areaOpacity: 0, color: "#47808d", lineWidth: 1},'+\
     '                   4: {areaOpacity: 0.02, color: "black", lineWidth: 1, lineDashStyle:[10,2] },'+\
     '                   5: {areaOpacity: 0.02, color: "black", lineWidth: 1, lineDashStyle:[10,2] }'+\
     '                  },'+\
