c8d2bfa2a0103f93e292bda82ba999d562101c31
daivieth <daiviet.huynh@gmail.com>
2018-12-23 20:46:15 +0700

    dev

--- signal_ta_chart_alt_orders.py
+++ signal_ta_chart_alt_orders.py
@@ -170,12 +170,12 @@ def get_ta_chart(uid):
     '          vAxis: { textStyle: {fontSize:'+ str(chart_font_size) +'}, gridlines: { color: "transparent" } },'+\
     '          hAxis: {textStyle: {fontSize:'+ str(chart_font_size) +'}, gridlines: { count: 4 } }, '+\
     '          series:{'+\
-    '                   0: {areaOpacity: 0.2, color: "#3f84c5", lineWidth: 3},'+\
+    '                   0: {areaOpacity: 0.2, color: "#3f84c5", lineWidth: 1},'+\
     '                   1: {areaOpacity: 0.2, color: "#ff9800", lineWidth: 3},'+\
-    '                   2: {areaOpacity: 0, color: "#47808d", lineWidth: 1},'+\
-    '                   3: {areaOpacity: 0, color: "#47808d", lineWidth: 1},'+\
-    '                   4: {areaOpacity: 0.05, color: "black", lineWidth: 1, lineDashStyle:[10,2] },'+\
-    '                   5: {areaOpacity: 0.05, color: "black", lineWidth: 1, lineDashStyle:[10,2] }'+\
+    '                   2: {areaOpacity: 0, color: "#47808d", lineWidth: 2},'+\
+    '                   3: {areaOpacity: 0, color: "#47808d", lineWidth: 2},'+\
+    '                   4: {areaOpacity: 0.05, color: "black", lineWidth: 2, lineDashStyle:[10,2] },'+\
+    '                   5: {areaOpacity: 0.05, color: "black", lineWidth: 2, lineDashStyle:[10,2] }'+\
     '                  },'+\
     '          chartArea:{width:"80%",height:"80%"}'+\
     '        };'+\
