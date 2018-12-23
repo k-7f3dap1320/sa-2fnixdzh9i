f5e3db5be56a61eacd2120b09317e5ae5bbf6865
daivieth <daiviet.huynh@gmail.com>
2018-12-23 19:41:29 +0700

    dev

--- signal_ta_chart_alt_orders.py
+++ signal_ta_chart_alt_orders.py
@@ -174,8 +174,6 @@ def get_ta_chart(uid):
     '          series:{1: {areaOpacity: 0.1, color: "#ecd0aa", lineWidth: 2} },'+\
     '          series:{2: {areaOpacity: 0, color: "#47808d", lineWidth: 1} },'+\
     '          series:{3: {areaOpacity: 0, color: "#47808d", lineWidth: 1} },'+\
-    '          series:{4: {areaOpacity: 0.02, color: "black", lineWidth: 1 } },'+\
-    '          series:{5: {areaOpacity: 0.02, color: "black", lineWidth: 1 } },'+\
     '          chartArea:{width:"80%",height:"80%"}'+\
     '        };'+\
     '        var chart = new google.visualization.AreaChart(document.getElementById("ta_chart"));'+\
@@ -184,6 +182,11 @@ def get_ta_chart(uid):
     "</script>"+\
     '<div id="ta_chart" class="sa-chart-hw-100"></div>'
 
+    '''
+    '          series:{4: {areaOpacity: 0.02, color: "black", lineWidth: 1, lineDashStyle:[10,2] } },'+\
+    '          series:{5: {areaOpacity: 0.02, color: "black", lineWidth: 1, lineDashStyle:[10,2] } },'+\
+    '''
+    
     return r
 
 
