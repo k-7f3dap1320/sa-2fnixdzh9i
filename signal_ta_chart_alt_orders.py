e0b56f5d5a078d0ff6bfeec1554d3f0780df87ba
daivieth <daiviet.huynh@gmail.com>
2018-12-23 20:29:28 +0700

    dev

--- signal_ta_chart_alt_orders.py
+++ signal_ta_chart_alt_orders.py
@@ -106,7 +106,8 @@ def get_ta_chart(uid):
     connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     cr = connection.cursor(pymysql.cursors.SSCursor)
     sql = "SELECT symbol, date, price_close, forecast, lt_upper_trend_line, lt_lower_trend_line, "+\
-    "st_upper_trend_line, st_lower_trend_line, ma200 FROM chart_data WHERE uid=" + str(uid)
+    "st_upper_trend_line, st_lower_trend_line, ma200 FROM chart_data WHERE uid=" + str(uid) +" "+\
+    "ORDER BY date "
     cr.execute(sql)
     rs = cr.fetchall()
     data = ''
