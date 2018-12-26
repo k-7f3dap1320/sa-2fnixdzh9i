5ba012ca16774af3f0503630645702ff65afe1cf
daivieth <daiviet.huynh@gmail.com>
2018-12-26 21:24:04 +0700

    dev

--- portf_alloc.py
+++ portf_alloc.py
@@ -151,7 +151,7 @@ def get_portf_alloc(uid):
         '                           pieSliceText: "percentage",'+\
         '                           slices: {' + pie_chart_option  + '},'+\
         '                           legend: {position:"labeled", textStyle: {fontSize: '+ str(pie_chart_font_size) +'} },'+\
-        '                           chartArea:{width:"80%",height:"80%"}'+\
+        '                           chartArea:{width:"90%",height:"80%"}'+\
         '                       };'+\
         '                       var chart = new google.visualization.PieChart(document.getElementById("portf_alloc_pie_chart"));'+\
         '                       chart.draw(data, options);'+\
