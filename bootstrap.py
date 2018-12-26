731d2031b20d1e0e8897008a180365b18f30fad0
daivieth <daiviet.huynh@gmail.com>
2018-12-26 10:38:39 +0700

    dev

--- bootstrap.py
+++ bootstrap.py
@@ -9,7 +9,8 @@ def get_bootstrap():
     popper = '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>'
     bootcss = '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">'
     bootsmin = '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>'
+    tooltip ="<script>$(function () {$('[data-toggle=\"tooltip\"]').tooltip()})</script>"
 
-    r = jquery + popper + bootcss + bootsmin
+    r = jquery + popper + bootcss + bootsmin + tooltip
 
     return r
