5899610e8a29b608fbbc538a28f152f06239d7e7
daivieth <daiviet.huynh@gmail.com>
2018-12-29 18:35:10 +0700

    'dev'

--- trail_returns_chart.py
+++ trail_returns_chart.py
@@ -9,6 +9,47 @@ import pymysql.cursors
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
+
+def get_chart_data(uid,p):
+    connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+    cr = connection.cursor(pymysql.cursors.SSCursor)
+    sql = "SELECT instruments.fullname,instruments.y1, instruments.m6, instruments.m3, instruments.m1, instruments.w1, instruments.unit, instruments.is_benchmark "+\
+    "FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol WHERE symbol_list.uid=" + str(uid)
+    cr.execute(sql)
+    rs = cr.fetchall()
+
+    for row in rs:
+        fullname = row[0]
+        y1 = row[1]
+        m6 = row[2]
+        m3 = row[3]
+        m1 = row[4]
+        w1 = row[5]
+        unit = row[6]
+        is_benchmark = row[7]
+
+    if unit == '%':
+        y1 = round( y1*100 ,2 )
+        m6 = round( m6*100 ,2 )
+        m3 = round( m3*100 ,2 )
+        m1 = round( m1*100 ,2 )
+        w1 = round( w1*100 ,2 )
+    cr.close()
+    connection.close()
+
+    if p == 'y1':
+        data = str(y1) + ',"' + str(y1) +' '+ str(unit)  + '"'
+    if p == 'm6':
+        data = str(m6) + ',"' + str(m6) +' '+ str(unit)  + '"'
+    if p == 'm3':
+        data = str(m3) + ',"' + str(m3) +' '+ str(unit)  + '"'
+    if p == 'm1':
+        data = str(m1) + ',"' + str(m1) +' '+ str(unit)  + '"'
+    if p == 'w1':
+        data = str(w1) + ',"' + str(w1) +' '+ str(unit)  + '"'
+
+    return data
+
 def get_trailing_returns(uid):
 
     connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
