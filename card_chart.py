6024e6323e2406c6fe3103612995d6e76e6fb661
daivieth <daiviet.huynh@gmail.com>
2018-12-31 09:28:34 +0700

    dev

--- card_chart.py
+++ card_chart.py
@@ -31,7 +31,7 @@ def write_func(uid,data,color):
     "        }"+\
     "      },"+\
     "      vAxis: {"+\
-    "        viewWindow:{min: 1000, viewWindowMode: "explicit"},minValue:auto,"+\
+    "        viewWindow:{min: 1000, viewWindowMode: 'explicit'},minValue:auto,"+\
     "        title: '',"+\
     "        gridlines: {"+\
     "            color: 'transparent'"+\
