0454b199e1857e2599857e2716099d3bf4aa6646
daivieth <daiviet.huynh@gmail.com>
2018-12-31 09:41:25 +0700

    dev

--- portf_perf_desc.py
+++ portf_perf_desc.py
@@ -90,7 +90,7 @@ def get_perf_chart(uid):
     connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     cr = connection.cursor(pymysql.cursors.SSCursor)
 
-    sql = "SELECT price_close FROM chart_data WHERE uid="+ str(uid) + " ORDER BY price_close DESC LIMIT 1"
+    sql = "SELECT price_close FROM chart_data WHERE uid="+ str(uid) + " ORDER BY price_close LIMIT 1"
     cr.execute(sql)
     rs = cr.fetchall()
     for row in rs:
