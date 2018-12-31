8cddec89035067b40cf2b9c496ec33b52ee9cc54
daivieth <daiviet.huynh@gmail.com>
2018-12-31 18:26:56 +0700

    dev

--- card_chart.py
+++ card_chart.py
@@ -63,19 +63,21 @@ def get_card_chart(uid,color):
         for row in rs:
             minval = row[0]
 
-        sql = "SELECT date, price_close FROM chart_data WHERE date>="+str(d)+" AND uid="+ str(uid)+" ORDER BY date"
+        sql = "SELECT date, price_close, forecast FROM chart_data WHERE date>="+str(d)+" AND uid="+ str(uid)+" ORDER BY date"
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs:
             date = row[0]
             price_close = row[1]
+            forecast = row[2]
             year = date.strftime("%Y")
             month = date.strftime("%m")
             day = date.strftime("%d")
-            if data =="":
-                data = data + "[new Date("+str(year)+", "+str( int(month) -1 )+", "+str(day)+"),"+str(price_close)+"]"
-            else:
-                data = data + ",[new Date("+str(year)+", "+str( int(month) -1 )+", "+str(day)+"),"+str(price_close)+"]"
+            if forecast == 0:
+                if data =="":
+                    data = data + "[new Date("+str(year)+", "+str( int(month) -1 )+", "+str(day)+"),"+str(price_close)+"]"
+                else:
+                    data = data + ",[new Date("+str(year)+", "+str( int(month) -1 )+", "+str(day)+"),"+str(price_close)+"]"
         cr.close()
         connection.close()
     except Exception as e: print(e)
