9204b946fd1b3cc6bcf874b8c1d1254657446ff5
daivieth <daiviet.huynh@gmail.com>
2019-01-12 21:29:28 +0700

    dev

--- createuser_form.py
+++ createuser_form.py
@@ -64,7 +64,7 @@ def get_user_creation_form(burl):
         '</div>'
 
 
-        box_content = '<div class="box">' +\
+        box_content = box_content + '<div class="box">' +\
         '   <div class="row">'+\
         '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
         '            <div class="box-part">'+\
