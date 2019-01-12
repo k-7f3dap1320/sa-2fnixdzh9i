954d110162c302e9e15aad45ca0c065015010af3
daivieth <daiviet.huynh@gmail.com>
2019-01-12 14:59:39 +0700

    dev

--- signal_main.py
+++ signal_main.py
@@ -39,7 +39,7 @@ def gen_sign_page(uid,appname,burl):
             instfullname = row[0]
 
         r = get_head(  get_loading_head() + get_title( appname +' - ' + instfullname ) + get_metatags(burl) + get_bootstrap() + get_awesomplete() + get_google_chart_script() + get_stylesheet(burl) )
-        r = r + get_body( get_loading_body(), navbar(burl) + '<div class="box"><div class="row">' + get_details_header(uid) + get_sign_header(uid) + get_sign_ta_chart_alt_orders(uid) + get_sign_recommend_trail_returns(uid) + get_trades_box(uid,burl) + '</div></div>')
+        r = r + get_body( get_loading_body(), navbar(burl) + '<div class="box-top"><div class="row">' + get_details_header(uid) + get_sign_header(uid) + get_sign_ta_chart_alt_orders(uid) + get_sign_recommend_trail_returns(uid) + get_trades_box(uid,burl) + '</div></div>')
         r = set_page(r)
 
         cr.close()
