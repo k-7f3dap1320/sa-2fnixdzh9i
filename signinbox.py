ae493c1a807607ef9da20f9838463f74ef693e89
daivieth <daiviet.huynh@gmail.com>
2019-01-12 14:05:50 +0700

    dev

--- signinbox.py
+++ signinbox.py
@@ -12,9 +12,9 @@ def get_signin_box(burl):
 
         box_content = '<div class="box"><div class="row">' +\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part sa-signin-box">'+\
-        '                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
-        '                   <h1>A.I. Powered Trading Intelligence</h1>   '+\
+        '            <div class="box-part">'+\
+        '                <div class="col-lg-6 col-md-6 col-sm-23 col-xs-12 sa-signin-box">'+\
+        '                   <h2>A.I. powered Trading Insight</h2>   '+\
         '                   <div>&nbsp;</div>'+\
         '                </div>'+\
         '                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
