a7a6c8bcd0825272ef5adfa9fb9e1fc3b22f4e2f
daivieth <daiviet.huynh@gmail.com>
2019-01-12 21:27:56 +0700

    dev

--- createuser_form.py
+++ createuser_form.py
@@ -12,49 +12,38 @@ def get_user_creation_form(burl):
     try:
 
         box_content = '<div class="box-top">' +\
-        '   <div class="row">'+\
+        '   <div>'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
         '            <div class="box-part disable-overflow">'+\
-        '     <form class="form-horizontal" role="form" method="POST" action="/register">'+\
-        '         <div class="row">'+\
-        '             <div class="col-md-3"></div>'+\
-        '            <div class="col-md-6">'+\
+        '     <form method="POST" action="'+ burl +'n/?uid=xxx">'+\
+        '         <div>'+\
+        '            <div>'+\
         '                <h3 style="text-align: center;">Join SmartAlpha today.</h3>'+\
         '                <hr>'+\
         '            </div>'+\
         '        </div>'+\
-        '        <div class="row">'+\
-        '            <div class="col-md-3 field-label-responsive">'+\
-        '            </div>'+\
-        '            <div class="col-md-6">'+\
+        '        <div>'+\
+        '            <div>'+\
         '                <div class="form-group">'+\
         '                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">'+\
         '                        <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-user"></i></div>'+\
-        '                        <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required autofocus>'+\
+        '                        <input type="text" name="name" class="form-control" id="name" placeholder="Your name" required autofocus>'+\
         '                    </div>'+\
         '                </div>'+\
         '            </div>'+\
-        '            <div class="col-md-3">'+\
-        '            </div>'+\
         '        </div>'+\
-        '        <div class="row">'+\
-        '            <div class="col-md-3 field-label-responsive">'+\
-        '            </div>'+\
-        '            <div class="col-md-6">'+\
+        '        <div>'+\
+        '            <div>'+\
         '                <div class="form-group">'+\
         '                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">'+\
         '                        <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>'+\
-        '                        <input type="text" name="email" class="form-control" id="email" placeholder="your email: you@example.com" required autofocus>'+\
+        '                        <input type="text" name="email" class="form-control" id="email" placeholder="Your email: you@example.com" required autofocus>'+\
         '                    </div>'+\
         '                </div>'+\
         '            </div>'+\
-        '            <div class="col-md-3">'+\
-        '            </div>'+\
         '        </div>'+\
-        '        <div class="row">'+\
-        '            <div class="col-md-3 field-label-responsive">'+\
-        '            </div>'+\
-        '            <div class="col-md-6">'+\
+        '        <div>'+\
+        '            <div>'+\
         '                <div class="form-group has-danger">'+\
         '                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">'+\
         '                        <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>'+\
@@ -63,13 +52,22 @@ def get_user_creation_form(burl):
         '                </div>'+\
         '            </div>'+\
         '        </div>'+\
-        '        <div class="row">'+\
-        '            <div class="col-md-3"></div>'+\
-        '            <div class="col-md-6">'+\
-        '                <button type="submit" class="btn btn-success btn-lg btn-block form-signin-btn"><i class="fa fa-user-plus"></i> Register</button>'+\
+        '        <div>'+\
+        '            <div>'+\
+        '                <button type="submit" class="btn btn-success btn-lg btn-block form-signin-btn"><i class="fa fa-user-plus"></i> Sign up</button>'+\
         '            </div>'+\
         '        </div>'+\
         '    </form>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '   </div>'+\
+        '</div>'
+
+
+        box_content = '<div class="box">' +\
+        '   <div class="row">'+\
+        '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
+        '            <div class="box-part">'+\
         '            </div>'+\
         '        </div>'+\
         '   </div>'+\
