186166806567ef8ae3321ab71c67acaf03244ab1
daivieth <daiviet.huynh@gmail.com>
2019-01-13 07:28:44 +0700

    dev

--- createuser_form.py
+++ createuser_form.py
@@ -28,7 +28,7 @@ def get_user_creation_form(burl):
         '                <div class="form-group">'+\
         '                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">'+\
         '                        <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-user"></i></div>'+\
-        '                        <input type="text" name="name" class="form-control" id="name" placeholder="Your name" required autofocus>'+\
+        '                        <input type="text" name="name" class="form-control btn-info" id="name" placeholder="Your name" required autofocus>'+\
         '                    </div>'+\
         '                </div>'+\
         '            </div>'+\
@@ -38,7 +38,7 @@ def get_user_creation_form(burl):
         '                <div class="form-group">'+\
         '                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">'+\
         '                        <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>'+\
-        '                        <input type="email" name="email" class="form-control" id="email" placeholder="Your email: you@example.com" required autofocus>'+\
+        '                        <input type="email" name="email" class="form-control btn-info" id="email" placeholder="Your email: you@example.com" required autofocus>'+\
         '                    </div>'+\
         '                </div>'+\
         '            </div>'+\
@@ -48,14 +48,14 @@ def get_user_creation_form(burl):
         '                <div class="form-group has-danger">'+\
         '                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">'+\
         '                        <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>'+\
-        '                        <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>'+\
+        '                        <input type="password" name="password" class="form-control btn-info" id="password" placeholder="Password" required>'+\
         '                    </div>'+\
         '                </div>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div>'+\
         '            <div>'+\
-        '                <button type="submit" class="btn btn-success btn-lg btn-block form-signin-btn"><i class="fa fa-user-plus"></i> Sign up</button>'+\
+        '                <button type="submit" class="btn btn-info btn-lg btn-block form-signin-btn"><i class="fa fa-user-plus"></i> Sign up</button>'+\
         '            </div>'+\
         '        </div>'+\
         '    </form>'+\
