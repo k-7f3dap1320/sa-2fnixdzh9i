0a77064ebef41923a7ff17fe2763843d57b0c0f1
daivieth <daiviet.huynh@gmail.com>
2019-01-16 19:47:46 +0700

    dev

--- app_cookie.py
+++ app_cookie.py
@@ -2,6 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+from flask import make_response
 from app_page import *
 from app_head import *
 from app_body import *
