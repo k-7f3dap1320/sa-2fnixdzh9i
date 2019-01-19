197ef742ea96431b6aa17096cbc000ff6291c9ee
daivieth <daiviet.huynh@gmail.com>
2019-01-19 17:37:55 +0700

    dev

--- application.py
+++ application.py
@@ -55,7 +55,7 @@ def go():
         nickname = request.values.get('nickname')
         step = request.args.get('step')
         if step == 'a': c = gen_createuser_avatar(appname,burl,err)
-        elif step == 'b': c = save_avatar(burl,nickname,err)
+        elif step == 'b': c = save_avatar(burl,nickname)
         elif step == 'c': c = gen_selectmarket_page(appname,burl)
         elif step == 'd': c= save_selectmarket(burl,x)
         else: c = gen_createuser_page(uid,appname,burl,name,username,password)
