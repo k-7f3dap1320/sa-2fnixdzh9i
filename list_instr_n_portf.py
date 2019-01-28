bed7ac27f7222d5f4d2421938b80a674bfcb44d4
daivieth <daiviet.huynh@gmail.com>
2019-01-28 09:24:53 +0700

    dev

--- list_instr_n_portf.py
+++ list_instr_n_portf.py
@@ -36,6 +36,8 @@ def get_table_content_list_instr_n_portf(burl,mode,what,step,portf):
                 m3_signal = row[6]; m1_signal = row[7]
                 w1_signal = row[8]; w_forecast_display_info = row[9]
                 unit = row[10], symbol = row[11]
+            cr.close()
+            connection.close()
 
                 volatility_risk_st = str(round(volatility_risk_st*100),2)
 
