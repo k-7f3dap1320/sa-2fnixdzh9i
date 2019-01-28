f2cb8689349c440430d7e67e93d823d96a0df837
daivieth <daiviet.huynh@gmail.com>
2019-01-28 23:44:38 +0700

    dev

--- list_instr_n_portf.py
+++ list_instr_n_portf.py
@@ -3,13 +3,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from sa_db import *
+from sa_func import *
 access_obj = sa_db_access()
 import pymysql.cursors
 
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
-def get_table_content_list_instr_n_portf(burl,mode,what,step,portf,maxrow):
+def get_table_content_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x):
 
     r = '<script>$(document).ready(function($) {'+\
         '$(".sa-table-click-row").click(function() {'+\
@@ -18,13 +19,14 @@ def get_table_content_list_instr_n_portf(burl,mode,what,step,portf,maxrow):
         '});</script>'
     try:
 
+        if x is None: x = get_user_default_profile()
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
         if what == 'instr':
             sql = "SELECT symbol_list.uid, instruments.w_forecast_change, instruments.fullname, instruments.volatility_risk_st, "+\
             "instruments.y1_signal, instruments.m6_signal, instruments.m3_signal, instruments.m1_signal, instruments.w1_signal, "+\
             "instruments.w_forecast_display_info, instruments.unit, instruments.symbol FROM instruments JOIN symbol_list ON instruments.symbol = symbol_list.symbol "+\
-            "WHERE symbol_list.symbol NOT LIKE '%PRF:%' AND instruments.y1_signal > 0 AND ( instruments.market = 'FX:' OR instruments.asset_class = 'FX:') "+\
+            "WHERE symbol_list.symbol NOT LIKE '%"+ str( get_portf_suffix() ) +"%' AND instruments.y1_signal > 0 AND ( instruments.market = '"+ str(x) +"' OR instruments.asset_class = '"+ str(x) +"') "+\
             "ORDER BY RAND() LIMIT "+ str(maxrow)
             print(sql)
             cr.execute(sql)
@@ -79,7 +81,7 @@ def get_table_content_list_instr_n_portf(burl,mode,what,step,portf,maxrow):
     return r
 
 
-def gen_instr_n_portf_table(burl,mode,what,step,portf,maxrow):
+def gen_instr_n_portf_table(burl,mode,what,step,portf,maxrow,x):
 
     r = ''
     try:
@@ -100,7 +102,7 @@ def gen_instr_n_portf_table(burl,mode,what,step,portf,maxrow):
         '    </tr>'+\
         ' </thead>'+\
         '  <tbody>'+\
-        get_table_content_list_instr_n_portf(burl,mode,what,step,portf,maxrow) +\
+        get_table_content_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x) +\
         '  </tbody>'+\
         '</table>'
 
@@ -108,7 +110,7 @@ def gen_instr_n_portf_table(burl,mode,what,step,portf,maxrow):
         print(e)
     return r
 
-def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow):
+def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x):
     # mode = 'view', mode = 'portf_select'
     # what = 'instr', what = 'portf'
     # portf = portf uid
@@ -121,7 +123,7 @@ def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow):
         '   <div class="row">'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
         '            <div class="box-part sa-center-content sa-list-select-100pct">'+\
-        gen_instr_n_portf_table(burl,mode,what,step,portf,maxrow)
+        gen_instr_n_portf_table(burl,mode,what,step,portf,maxrow,x)
         '            </div>'+\
         '        </div>'+\
         '   </div>'+\
