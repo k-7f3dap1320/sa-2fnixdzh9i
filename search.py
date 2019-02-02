91884500930ac985affb7a8e67a7bcaccada34b8
daivieth <daiviet.huynh@gmail.com>
2019-02-02 14:02:35 +0700

    dev

--- search.py
+++ search.py
@@ -8,6 +8,7 @@ import pymysql.cursors
 from app_head import *
 from app_body import *
 from app_page import *
+from sa_func import *
 
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
@@ -15,9 +16,10 @@ db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access
 def get_search_suggestions():
 
     try:
+        portf_suffix = get_portf_suffix()
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT search FROM feed ORDER BY search"
+        sql = "SELECT search FROM feed WHERE search NOT LIKE '%"+ portf_suffix +"%' ORDER BY search"
         cr.execute(sql)
         rs = cr.fetchall()
         s = ''
