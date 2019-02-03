3f2e100634f861c57909f91268f1bfc718c0d840
daivieth <daiviet.huynh@gmail.com>
2019-02-03 08:22:19 +0700

    dev

--- createportf_main.py
+++ createportf_main.py
@@ -73,6 +73,12 @@ def get_selectportf_box(burl,step,mode,x):
             progress_value = '95'
         l_desc_part_2 = "Find and pick an item from the list below"
 
+        portf_selection = ''
+        for i in range(5):
+            select_instr = get_portf_select(i)
+            if not select_instr == '':
+                portf_selection = portf_selection + '<h4><span class="badge badge-secondary">'+ select_instr +'</span>&nbsp;&nbsp;</h4>'
+
         box_content = '<div class="box-top">' +\
         '   <div class="row">'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
@@ -87,6 +93,7 @@ def get_selectportf_box(burl,step,mode,x):
         '            </div>'+\
         '        </div>'+\
         '   </div>'+\
+        portf_selection +\
         '</div>'
 
     except Exception as e: print(e)
@@ -96,10 +103,42 @@ def save_portf_select(appname,burl,step,mode,x,portf,uid):
     r = ''
     try:
         next_step = int(step) +1
-        r = gen_selectportf_page(appname,burl,next_step,mode,x,portf)
+        if step < 5:
+            resp = make_response( redirect(burl+'p/?ins=1&step='+ str(next_step) ) )
+            resp.set_cookie('portf_s_'+str(step), str(uid), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+        else:
+            resp = make_response( redirect(burl+'p/?ins=3' ) )
 
-        resp = make_response( redirect(burl+'p/?ins=1&step='+ str(next_step) ) )
-        resp.set_cookie('portf_s_'+str(step), str(uid), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+    except Exception as e:
+        print(e)
+    return resp
+
+def get_portf_select(select):
+    r = ''
+    try:
+        uid = return request.cookies.get('portf_s_' + str(select) )
+
+        if not uid is None or uid == '':
+            connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+            cr = connection.cursor(pymysql.cursors.SSCursor)
+            sql = "SELECT instruments.fullname FROM instruments JOIN symbol_list ON instruments.symbol = symbol_list.symbol "+\
+            "WHERE symbol_list.uid=" + str(uid)
+            cr.execute(sql)
+            rs = cr.fetchall()
+            for row in rs: r = row[0]
+            cr.close()
+
+    except Exception as e:
+        print(e)
+    return r
+
+
+def ini_portf_select(r):
+    try:
+
+        resp = make_response(r)
+        for i in range(5):
+            resp.set_cookie('portf_s_' + str(i),'0',expires=datetime.datetime.now() + datetime.timedelta(days=1) )
 
     except Exception as e:
         print(e)
@@ -112,5 +151,6 @@ def gen_selectportf_page(appname,burl,step,mode,x,portf):
         r = get_head( get_loading_head() + get_googleanalytics() + get_title( appname ) + get_metatags(burl) + get_bootstrap() + get_awesomplete() + get_tablesorter() + get_font_awesome() + get_stylesheet(burl) )
         r = r + get_body( get_loading_body(), navbar(burl) + get_selectportf_box(burl,step,mode,x) + get_box_list_instr_n_portf(burl,'portf_select','instr',step,portf,1000,x) )
         r = set_page(r)
+        ini_portf_select(r)
     except Exception as e: print(e)
     return r
