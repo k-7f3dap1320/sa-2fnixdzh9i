a3a4d397b35b567f459bfc05e6cf0984621c5b6a
daivieth <daiviet.huynh@gmail.com>
2019-02-09 08:09:38 +0700

    dev

--- portf_save.py
+++ portf_save.py
@@ -4,7 +4,6 @@
 # LICENSE file in the root directory of this source tree.
 from flask import Flask, make_response, request, redirect
 from sa_db import *
-from createportf_main import *
 access_obj = sa_db_access()
 import pymysql.cursors
 
@@ -33,6 +32,19 @@ def get_portf_table_rows():
 
             strategy_order_type = request.cookies.get('portf_s_type_' + str(i+1) )
             strategy_conviction = request.cookies.get('portf_s_conv_' + str(i+1) )
+            instr_selection = ''
+            uid = request.cookies.get('portf_s_' + str(i+1) )
+
+            if not uid is None or uid == '':
+                connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+                cr = connection.cursor(pymysql.cursors.SSCursor)
+                sql = "SELECT instruments.fullname, instruments.symbol FROM instruments JOIN symbol_list ON instruments.symbol = symbol_list.symbol "+\
+                "WHERE symbol_list.uid=" + str(uid)
+                cr.execute(sql)
+                rs = cr.fetchall()
+                for row in rs: instr_selection = row[0] + '('+ row[1] +')'
+                cr.close()
+
 
             r = r + ''+\
             '    <tr>'+\
@@ -60,7 +72,7 @@ def get_portf_table_rows():
             '           </div>'+\
             '       </div>'+\
             '      </td>'+\
-            '      <td width="100%">'+ get_portf_select(i+1) +'</td>'+\
+            '      <td width="100%">'+ instr_selection +'</td>'+\
             '    </tr>'
     except Exception as e:
         print(e)
