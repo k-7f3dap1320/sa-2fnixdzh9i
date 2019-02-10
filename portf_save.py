c0079deab01f563bd1c0395ad5a66318af1e856f
daivieth <daiviet.huynh@gmail.com>
2019-02-10 07:51:22 +0700

    dev

--- portf_save.py
+++ portf_save.py
@@ -42,7 +42,7 @@ def get_portf_table_rows():
                 "WHERE symbol_list.uid=" + str(uid)
                 cr.execute(sql)
                 rs = cr.fetchall()
-                for row in rs: instr_selection = row[0] + '('+ row[1] +')'
+                for row in rs: instr_selection = '<strong>' + row[0] + '</strong>&nbsp;('+ row[1] +')'
                 cr.close()
 
 
