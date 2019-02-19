0b3d4e372595aecb425bc6943002268ce1afd399
daivieth <daiviet.huynh@gmail.com>
2019-02-19 17:04:21 +0700

    dev

--- trades_tab.py
+++ trades_tab.py
@@ -65,8 +65,8 @@ def get_trades_tbl(uid,w):
             "trades.pnl_pct,  "+\
             "trades.url,  "+\
             "instruments.unit, "+\
-            "instruments.strategy_order_type"
-        sql = sql + "FROM trades JOIN instruments ON trades.symbol = instruments.symbol WHERE "
+            "portfolios.strategy_order_type"
+        sql = sql + "FROM trades JOIN portfolios ON portfolios.symbol = instruments.symbol JOIN instruments ON trades.symbol = instruments.symbol WHERE "
 
         if w == 'active': sql = sql + " trades.status = 'active' "
         else: sql = sql + " trades.status = 'expired' "
