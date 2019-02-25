c531445baa0ef5aed467bf7c2f23509bb4395682
daivieth <daiviet.huynh@gmail.com>
2019-02-25 19:35:38 +0700

    dev

--- portf_gen_data.py
+++ portf_gen_data.py
@@ -6,36 +6,133 @@
 import sys
 import os
 import datetime
+from sa_db import *
 import time
 from datetime import timedelta
+from sa_numeric import *
 
-pdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.abspath(pdir) )
-from settings import *
-sett = sa_path()
-
-sys.path.append(os.path.abspath( sett.get_path_pwd() ))
-from sa_access import *
 access_obj = sa_db_access()
+import pymysql.cursors
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
-sys.path.append(os.path.abspath( sett.get_path_feed() ))
-from add_feed_type import *
 
-sys.path.append(os.path.abspath( sett.get_path_core() ))
-from get_instr_perf_summ import *
-from sa_numeric import *
+def get_pct_from_date(d, sql_select, lp):
+    pct = 0
+    pp = 0
+    connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+    cr = connection.cursor(pymysql.cursors.SSCursor)
+    sql = sql_select + "AND date <= '"+ str(d) +"' ORDER BY date DESC LIMIT 1"
+    cr.execute(sql)
+    rs = cr.fetchall()
+    for row in rs:
+        pp = row[0]
+    cr.close()
+    connection.close()
 
-from pathlib import Path
+    if pp != 0:
+        pct = ( (lp - pp) / pp)
+    return pct
 
-import pymysql.cursors
-connection = pymysql.connect(host=db_srv,
-                             user=db_usr,
-                             password=db_pwd,
-                             db=db_name,
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+
+class instr_sum_data:
+    s = ""
+    uid = ""
+    sql_select = ""
+    sql_select_signal = ""
+    ld = datetime.datetime(2000, 1, 1, 1, 1)
+    d_1Yp = datetime.datetime(2000, 1, 1, 1, 1)
+    d_6Mp = datetime.datetime(2000, 1, 1, 1, 1)
+    d_3Mp = datetime.datetime(2000, 1, 1, 1, 1)
+    d_1Mp = datetime.datetime(2000, 1, 1, 1, 1)
+    d_1Wp = datetime.datetime(2000, 1, 1, 1, 1)
+    d_1Wf = datetime.datetime(2000, 1, 1, 1, 1)
+    lp = 0
+    lp_signal = 0
+
+    def __init__(self,symbol,uid):
+        self.s = symbol
+        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT symbol from symbol_list WHERE uid=" + str(uid)
+        cr.execute(sql)
+        rs = cr.fetchall()
+        for row in rs: symbol_is_portf = row[0]
+        if symbol_is_portf.find( get_portf_suffix() ) > -1 :
+            self.sql_select = "SELECT price_close, date FROM chart_data WHERE symbol='"+ self.s +"' "
+        else:
+            self.sql_select = "SELECT price_close, date FROM price_instruments_data WHERE symbol='"+ self.s + "' "
+            self.sql_select_signal = "SELECT signal_price, date from chart_data WHERE symbol='"+ self.s +"' AND forecast = 0 "
+            sql = self.sql_select_signal+" ORDER BY Date DESC LIMIT 1"
+            cr.execute(sql)
+            rs = cr.fetchall()
+            for row in rs: self.lp_signal = row[0];
+
+
+        sql = self.sql_select+" ORDER BY Date DESC LIMIT 1"
+        cr.execute(sql)
+        rs = cr.fetchall()
+        for row in rs: self.lp = row[0]; self.ld = row[1]
+        cr.close()
+        connection.close()
+
+        self.uid = uid
+        self.d_1Yp = self.ld - ( timedelta(days=365) )
+        self.d_6Mp = self.ld - ( timedelta(days=180) )
+        self.d_3Mp = self.ld - ( timedelta(days=90) )
+        self.d_1Mp = self.ld - ( timedelta(days=30) )
+        self.d_1Wp = self.ld - ( timedelta(days=7) )
+        self.d_1Wf = 0
+
+    def get_uid(self):
+        return self.uid
+
+    def get_lp(self):
+        return self.lp
+
+    def get_ticker(self):
+        return self.s
+
+    def get_pct_1Yp(self):
+        str_date = self.d_1Yp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select, self.lp))
+
+    def get_pct_6Mp(self):
+        str_date = self.d_6Mp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select, self.lp))
+
+    def get_pct_3Mp(self):
+        str_date = self.d_3Mp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select, self.lp))
+
+    def get_pct_1Mp(self):
+        str_date = self.d_1Mp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select, self.lp))
+
+    def get_pct_1Wp(self):
+        str_date = self.d_1Wp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select, self.lp))
+
+    def get_pct_1Yp_signal(self):
+        str_date = self.d_1Yp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select_signal, self.lp_signal))
+
+    def get_pct_6Mp_signal(self):
+        str_date = self.d_6Mp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select_signal, self.lp_signal))
+
+    def get_pct_3Mp_signal(self):
+        str_date = self.d_3Mp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select_signal, self.lp_signal))
+
+    def get_pct_1Mp_signal(self):
+        str_date = self.d_1Mp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select_signal, self.lp_signal))
+
+    def get_pct_1Wp_signal(self):
+        str_date = self.d_1Wp.strftime("%Y%m%d")
+        return str(get_pct_from_date(str_date, self.sql_select_signal, self.lp_signal))
+
 
 def get_portf_perf_summ(s,uid):
     pps = instr_sum_data(s, uid)
@@ -47,6 +144,7 @@ def get_portf_perf_summ(s,uid):
     romad_st = get_romad(sql)
     volatility_risk_st = get_volatility_risk(sql,True,s)
 
+    connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     cr = connection.cursor(pymysql.cursors.SSCursor)
     sql = "UPDATE instruments SET y1="+ str(y1) +", m6="+ str(m6) +", m3="+ str(m3) +", m1="+ str(m1) +", w1="+ str(w1) +", "+\
     " stdev_st="+ str(stdev_st) + ", maximum_dd_st="+ str(maximum_dd_st) + ", romad_st="+ str(romad_st) + ", volatility_risk_st="+ str(volatility_risk_st) +\
@@ -54,11 +152,13 @@ def get_portf_perf_summ(s,uid):
     cr.execute(sql)
     connection.commit()
     cr.close()
+    connection.close()
 
 def get_portf_perf(s):
     portf_symbol_suffix = get_portf_suffix()
     df = datetime.datetime.now() - timedelta(days=370)
 
+    connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     cr = connection.cursor(pymysql.cursors.SSCursor)
     sql = "SELECT symbol_list.symbol, symbol_list.uid, instruments.fullname, instruments.account_reference "+\
     "FROM `symbol_list` INNER JOIN instruments ON symbol_list.symbol = instruments.symbol "+\
@@ -142,3 +242,4 @@ def get_portf_perf(s):
         get_portf_perf_summ(portf_symbol, portf_uid)
 
     cr.close()
+    connection.close()
---
+++ sa_numeric.py
@@ -0,0 +1,148 @@
+# Copyright (c) 2018-present, Taatu Ltd.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import sys
+import os
+from pathlib import Path
+import numpy as np
+import math as math
+from math import exp, expm1
+from sa_db import *
+
+access_obj = sa_db_access()
+db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
+
+import pymysql.cursors
+
+connection = pymysql.connect(host=db_srv,
+                             user=db_usr,
+                             password=db_pwd,
+                             db=db_name,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+def get_pct_change(ini_val,new_val):
+
+    if not new_val == 0:
+        if new_val < ini_val:
+            r =  ( (ini_val - new_val) / ini_val ) * (-1)
+        else:
+            r = (new_val - ini_val) / new_val
+    else:
+        r = 0
+
+    return r
+
+
+def get_stdev(sql):
+
+    r = 0
+    try:
+        #sql with just one numerical value to compute standard deviation
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        cr.execute(sql)
+        a = list( cr.fetchall() )
+        r = np.std(a)
+        print('stdev='+str(r) )
+        cr.close()
+    except Exception as e: print(e)
+
+    return r
+
+def get_volatility_risk(sql,is_portf,s):
+
+    r = 0
+
+    try:
+        #sql with one numerical column to compute volatility risk
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+
+        if is_portf:
+            sql_i = "SELECT account_reference FROM instruments WHERE symbol='"+ s +"'"
+            cr.execute(sql_i)
+            rs = cr.fetchall()
+            for row in rs: lp = row[0]
+            cr.close()
+        else:
+            cr.execute(sql)
+            rs = cr.fetchall()
+            for row in rs: lp = row[0]
+            cr.close()
+
+        stdev = get_stdev(sql)
+        rp = lp - stdev
+        r = abs( get_pct_change(lp,rp)  )
+    except Exception as e: print(e)
+
+    return r
+
+
+def get_mdd(sql):
+
+    r = 0
+    try:
+        #sql with just one numerical value to compute maximum drawdown
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        cr.execute(sql)
+        rs = cr.fetchall()
+        top = 0
+        breset = math.pow(10,100)
+        bottom = breset
+        pct_dd = 0
+        cur_dd = 0
+        for row in rs:
+            val = row[0]
+
+            if val > top:
+                top = val
+                bottom = breset
+
+            if val < bottom:
+                bottom = val
+
+            if bottom < top:
+                cur_dd = abs( get_pct_change(bottom,top) )
+            else:
+                cur_dd = 0
+
+            if cur_dd > pct_dd:
+                pct_dd = cur_dd
+        cr.close()
+
+        r = pct_dd
+        print('mdd='+ str(r) )
+    except Exception as e: print(e)
+
+    return r
+
+def get_romad(sql):
+
+    r = 0
+    try:
+        #sql with one column as numerical value to compute return on maximum drawdown
+        #ordered by date ASC
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        cr.execute(sql)
+        rs = cr.fetchall()
+        i = 0
+        first = 0
+        last = 0
+        for row in rs:
+            if i == 0:
+                first = row[0]
+            last = row[0]
+            i += 1
+        cr.close()
+
+        print('f='+str(first) + ' l='+str(last) )
+
+        rt = get_pct_change(first,last)
+        dd = get_mdd(sql)
+
+        r = rt / dd
+        print('romad='+ str(r) )
+    except Exception as e: print(e)
+
+    return r
