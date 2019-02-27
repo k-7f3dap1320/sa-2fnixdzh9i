6ede0d59e9b93f5e2c8f3326109ab39075b96fb2
daivieth <daiviet.huynh@gmail.com>
2019-02-27 15:11:24 +0700

    dev

--- trades_tab.py
+++ trades_tab.py
@@ -32,7 +32,7 @@ def get_trades_tbl(uid,w):
         user_symbol_selection = ''
         i = 0
         if is_user_prf:
-            sql = "SELECT DISTINCT portfolios.symbol FROM instruments JOIN portfolios ON instruments.symbol = portfolios.portf_symbol WHERE instruments.owner = '"+ get_user() +"' "
+            sql = "SELECT DISTINCT portfolios.symbol FROM instruments JOIN portfolios ON instruments.symbol = portfolios.portf_symbol WHERE instruments.owner = "+ str(get_user_numeric_id()) +" "
             cr.execute(sql)
             rs = cr.fetchall()
             for row in rs:
