d8ad8896f814e85c0d21388ef368cec3ccb195aa
daivieth <daiviet.huynh@gmail.com>
2019-02-27 17:54:33 +0700

    dev

--- portf_popup.py
+++ portf_popup.py
@@ -44,7 +44,7 @@ def gen_portf_popup(uid,pop):
             '  });'+\
             '  </script>'+\
             ' <div class="modal" id="portf_popup">'+\
-            '    <div class="modal-dialog">'+\
+            '    <div class="modal-dialog modal-lg">'+\
             '      <div class="modal-content">'+\
             '        <div class="modal-header">'+\
             '          <h4 class="modal-title">'+ label_header +'</h4>'+\
