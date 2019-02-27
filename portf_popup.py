ad2ce2252a6e9a9aa0878814b5349c8ed097dabf
daivieth <daiviet.huynh@gmail.com>
2019-02-27 17:41:57 +0700

    dev

--- portf_popup.py
+++ portf_popup.py
@@ -7,6 +7,8 @@ from sa_db import *
 access_obj = sa_db_access()
 import pymysql.cursors
 
+db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
+
 def gen_portf_popup(uid,pop):
     r = ''
     try:
