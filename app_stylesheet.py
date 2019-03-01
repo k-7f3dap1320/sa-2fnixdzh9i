455d5d63d60b955b420018f3b2512200b0a11bbe
daivieth <daiviet.huynh@gmail.com>
2019-03-01 08:37:48 +0700

    dev

--- app_stylesheet.py
+++ app_stylesheet.py
@@ -38,6 +38,7 @@ def get_stylesheet(burl):
     sa_instr_n_portf_list = '.sa-instr-n-portf-list{margin-top: -10px;}'
     sa_box_h = '.sa-box-h{max-height: 400px;}'
     sa_signin_box = '.sa-signin-box{background-image:url('+ burl + 'static/sibg.gif); background-size:100% 100%; height: 300px; color: white; text-align: justify; padding: 20px; }'
+    sa_user_header = '.sa-user-header-box{background-image:url('+ burl + 'static/sibg.gif); background-size:100% 100%;}'
     sa_descr_box_sm = '.sa-descr-box-sm{font-size: 13px;}'
     sa_chart_hw_90 = '.sa-chart-hw-90{height: 90%;}'
     sa_chart_hw_100 = '.sa-chart-hw-100{height: 350px;}'
@@ -78,6 +79,7 @@ def get_stylesheet(burl):
     sa_instr_n_portf_list +\
     sa_box_h +\
     sa_signin_box +\
+    sa_user_header +\
     sa_search_input +\
     sa_descr_box_sm +\
     sa_chart_hw_90 +\
--- user_profile_header.py
+++ user_profile_header.py
@@ -28,7 +28,7 @@ def get_box_user_profile_header(burl):
 
 
             box_content = '<div class="box-top">' +\
-            '   <div class="row">'+\
+            '   <div class="row sa-user-header-box">'+\
             '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
             '            <div class="box-part sa-center-content">'+\
             '<br><br><br>'+\
