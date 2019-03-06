1d160784946a404eb05d8dbe3140ad127fad0cb3
daivieth <daiviet.huynh@gmail.com>
2019-03-06 23:08:21 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -9,7 +9,7 @@ def get_page_footer(burl):
 
     try:
 
-        box_content = '<div class="box text-info">' +\
+        box_content = '<div class="box">' +\
         '   <div class="row">'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
@@ -18,28 +18,28 @@ def get_page_footer(burl):
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html" target="_blank">About Us</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html" target="_blank" class="text-info">About Us</a></span>' +\
         '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">Contact Us</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank" class="text-info">Contact Us</a></span>' +\
         '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">SmartAlpha for Business</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank" class="text-info">SmartAlpha for Business</a></span>' +\
         '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#tc" target="_blank">Terms and Conditions</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#tc" target="_blank" class="text-info">Terms and Conditions</a></span>' +\
         '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
