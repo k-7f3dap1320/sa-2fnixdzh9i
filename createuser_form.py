e363bf45014367b428bf95797d7a1d66c2027d53
daivieth <daiviet.huynh@gmail.com>
2019-03-06 22:33:44 +0700

    dev

--- createuser_form.py
+++ createuser_form.py
@@ -84,33 +84,6 @@ def get_user_creation_form(burl):
         '   </div>'+\
         '</div>'
 
-
-        box_content = box_content + '<div class="box">' +\
-        '   <div class="row">'+\
-        '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded" style="padding: 30px; height:250px">'+\
-        '               <div>&nbsp;</div>'+\
-        '               <h5>Taatu Ltd.</h5>'+\
-        '               <div>27 Old Gloucester Street</div>'+\
-        '               <div>London</div>'+\
-        '               <div>WC1N 3AX</div>'+\
-        '               <div>United Kingdom</div>'+\
-        '               <div>&nbsp;</div>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded" style="padding: 30px; height:250px">'+\
-        '               <div>&nbsp;</div>'+\
-        '               <h5>Contact us, we love feedback</h5>'+\
-        '               <div>e: <a href="mailto:info@taatu.co">info@taatu.co</a></div>'+\
-        '               <div>w: <a href="http://taatu.co">www.taatu.co</a></div>'+\
-        '               <div>&nbsp;</div>'+\
-        '               <div>Do you have any problem to log in? Drop us an <a href="mailto:info@taatu.co">email</a> or talk with us on <a href="https://www.facebook.com/messages/t/taatuco" target="_blank">messenger</a>.</div>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '   </div>'+\
-        '</div>'
-
     except Exception as e: print(e)
 
     return box_content
