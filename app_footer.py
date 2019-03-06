be10d5fb56bfd6433a1e3a643dde2d8cebb2fa30
daivieth <daiviet.huynh@gmail.com>
2019-03-06 18:08:12 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -12,10 +12,11 @@ def get_page_footer(burl):
         box_content = '<div class="box">' +\
         '   <div class="row">'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part sa-center-content footer">'+\
+        '            <div class="sa-center-content footer">'+\
         '               <span><a href="'+ burl +'static/taatu/index.html" target="_blank">About Us</a></span>&nbsp;&nbsp;&nbsp;&nbsp;' +\
         '               <span><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">Contact Us</a></span>&nbsp;&nbsp;&nbsp;&nbsp;' +\
         '               <span><a href="'+ burl +'static/taatu/index.html#tc" target="_blank">Terms and Conditions</a></span>&nbsp;&nbsp;&nbsp;&nbsp;' +\
+        '<br /><br /><br /><br />'+\
         '            </div>'+\
         '        </div>'+\
         '   </div>'+\
