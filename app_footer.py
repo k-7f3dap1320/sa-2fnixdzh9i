47646d216c2e12f96bd5b5dc0d92dfc30075e775
daivieth <daiviet.huynh@gmail.com>
2019-03-06 21:54:51 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -11,15 +11,35 @@ def get_page_footer(burl):
 
         box_content = '<div class="box">' +\
         '   <div class="row">'+\
-        '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
+        '        <div class="col-lg-3 col-md-3 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><br /><strong>'+\
-        '               <span><a href="'+ burl +'static/taatu/index.html" target="_blank">About Us</a></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +\
-        '               <span><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">Contact Us</a></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +\
+        '               <span><a href="'+ burl +'static/taatu/index.html" target="_blank">About Us</a></span>' +\
+        '               <br /><br /><br /><br /></strong>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '        <div class="col-lg-3 col-md-3 col-sm-1 col-xs-1">'+\
+        '            <div class="sa-center-content footer">'+\
+        '               <br /><br /><br /><strong>'+\
+        '               <span><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">Contact Us</a></span>' +\
+        '               <br /><br /><br /><br /></strong>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '        <div class="col-lg-3 col-md-3 col-sm-1 col-xs-1">'+\
+        '            <div class="sa-center-content footer">'+\
+        '               <br /><br /><br /><strong>'+\
+        '               <span><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">SmartAlpha for Business</a></span>' +\
+        '               <br /><br /><br /><br /></strong>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '        <div class="col-lg-3 col-md-3 col-sm-1 col-xs-1">'+\
+        '            <div class="sa-center-content footer">'+\
+        '               <br /><br /><br /><strong>'+\
         '               <span><a href="'+ burl +'static/taatu/index.html#tc" target="_blank">Terms and Conditions</a></span>' +\
         '               <br /><br /><br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
+
         '   </div>'+\
         '</div>'
 
