df718f9e5c4e023fb521d1ea06e0aef014a1dd96
daivieth <daiviet.huynh@gmail.com>
2019-03-06 22:59:45 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -19,28 +19,28 @@ def get_page_footer(burl):
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
         '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html" target="_blank">About Us</a></span>' +\
-        '               <br /><br /><br /><br /></strong>'+\
+        '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
         '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">Contact Us</a></span>' +\
-        '               <br /><br /><br /><br /></strong>'+\
+        '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
         '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">SmartAlpha for Business</a></span>' +\
-        '               <br /><br /><br /><br /></strong>'+\
+        '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
         '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#tc" target="_blank">Terms and Conditions</a></span>' +\
-        '               <br /><br /><br /><br /></strong>'+\
+        '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '   <div class="row">'+\
@@ -49,12 +49,15 @@ def get_page_footer(burl):
         '            </div>'+\
         '        </div>'+\
         '   </div>'+\
+        '</div>'+\
+        '<div class="box">'+\
         '   <div class="row">'+\
         '      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">'+\
         '           <div class="sa-center-content">Made with <i class="fas fa-heart"></i> from London, Nairobi to Bangkok</div>'+\
         '      </div>'+\
         '   </div>'+\
-        '</div>'
+        '</div>'+\
+        '<br /><br /><br /><br />'
 
 
     except Exception as e: print(e)
