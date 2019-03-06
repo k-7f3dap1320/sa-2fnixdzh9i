cd09e850a737ce216467249157ff78b7bce2ed77
daivieth <daiviet.huynh@gmail.com>
2019-03-06 23:00:46 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -52,7 +52,7 @@ def get_page_footer(burl):
         '</div>'+\
         '<div class="box">'+\
         '   <div class="row">'+\
-        '      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">'+\
+        '      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
         '           <div class="sa-center-content">Made with <i class="fas fa-heart"></i> from London, Nairobi to Bangkok</div>'+\
         '      </div>'+\
         '   </div>'+\
