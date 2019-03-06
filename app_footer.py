e6a6415885bb6d0098bfc73b51d9a83df8405ea2
daivieth <daiviet.huynh@gmail.com>
2019-03-06 22:31:31 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -18,28 +18,28 @@ def get_page_footer(burl):
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span><a href="'+ burl +'static/taatu/index.html" target="_blank">About Us</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html" target="_blank">About Us</a></span>' +\
         '               <br /><br /><br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">Contact Us</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">Contact Us</a></span>' +\
         '               <br /><br /><br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">SmartAlpha for Business</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank">SmartAlpha for Business</a></span>' +\
         '               <br /><br /><br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span><a href="'+ burl +'static/taatu/index.html#tc" target="_blank">Terms and Conditions</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#tc" target="_blank">Terms and Conditions</a></span>' +\
         '               <br /><br /><br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
