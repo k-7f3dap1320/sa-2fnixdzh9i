152abc2bad79e9d780264febe8a2b3efe461fdf2
daivieth <daiviet.huynh@gmail.com>
2019-03-06 18:18:41 +0700

    dev

--- portf_main.py
+++ portf_main.py
@@ -46,7 +46,7 @@ def gen_portf_page(uid,appname,burl,pop):
             instfullname = row[0]
 
         r = get_head(  get_loading_head() + get_googleanalytics() + get_title( appname +' - ' + instfullname ) + get_metatags(burl) + get_bootstrap() + get_awesomplete() + get_tablesorter() + get_font_awesome() + get_google_chart_script() + get_stylesheet(burl) )
-        r = r + get_body(  get_loading_body(), gen_portf_popup(uid,pop) + navbar(burl) + '<div class="box-top"><div class="row">' + get_details_header(uid,burl) + get_portf_alloc(uid) + get_portf_perf_desc(uid) + get_portf_risk_trail_returns(uid) + get_trades_box(uid,burl) +  get_page_footer(burl) + '</div></div>' )
+        r = r + get_body(  get_loading_body(), gen_portf_popup(uid,pop) + navbar(burl) + '<div class="box-top"><div class="row">' + get_details_header(uid,burl) + get_portf_alloc(uid) + get_portf_perf_desc(uid) + get_portf_risk_trail_returns(uid) + get_trades_box(uid,burl) + '</div></div>' +  get_page_footer(burl)  )
         r = set_page(r)
 
         cr.close()
--- signal_main.py
+++ signal_main.py
@@ -43,7 +43,7 @@ def gen_sign_page(uid,appname,burl):
             instfullname = row[0]
 
         r = get_head(  get_loading_head() + get_googleanalytics() + get_title( appname +' - ' + instfullname ) + get_metatags(burl) + get_bootstrap() + get_awesomplete() + get_tablesorter() + get_font_awesome() + get_google_chart_script() + get_stylesheet(burl) )
-        r = r + get_body( get_loading_body(), navbar(burl) + '<div class="box-top"><div class="row">' + get_details_header(uid,burl) + get_sign_header(uid) + get_sign_ta_chart_alt_orders(uid) + get_sign_recommend_trail_returns(uid) + get_trades_box(uid,burl) + get_page_footer(burl) + '</div></div>')
+        r = r + get_body( get_loading_body(), navbar(burl) + '<div class="box-top"><div class="row">' + get_details_header(uid,burl) + get_sign_header(uid) + get_sign_ta_chart_alt_orders(uid) + get_sign_recommend_trail_returns(uid) + get_trades_box(uid,burl) + '</div></div>' + get_page_footer(burl))
         r = set_page(r)
 
         cr.close()
