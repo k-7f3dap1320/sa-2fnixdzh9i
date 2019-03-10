f9bac3498ab4e2c909d7798a696b0fb8a7029137
daivieth <daiviet.huynh@gmail.com>
2019-03-10 16:07:15 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -32,7 +32,7 @@ def get_page_footer(burl):
         '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
         '            <div class="sa-center-content footer">'+\
         '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank" class="text-info">SmartAlpha for Business</a></span>' +\
+        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#business" target="_blank" class="text-info">SmartAlpha for Business</a></span>' +\
         '               <br /><br /></strong>'+\
         '            </div>'+\
         '        </div>'+\
