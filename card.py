c4320d0a23f983f649d3e60c2df55900af7f849d
daivieth <daiviet.huynh@gmail.com>
2019-03-11 09:48:05 +0700

    dev

--- card.py
+++ card.py
@@ -19,23 +19,30 @@ def get_card(x,t,burl):
     try:
         if t == 1:
             sql = "SELECT short_title, short_description, content, url, ranking, badge, symbol FROM feed "+\
-            "WHERE (asset_class LIKE '%"+x+"%' OR market LIKE '"+x+"') AND type=1 ORDER BY ranking DESC LIMIT 28"
+            "WHERE (asset_class LIKE '%"+x+"%' OR market LIKE '%"+x+"%') AND type=1 ORDER BY ranking DESC LIMIT 20"
         if t == 9:
             if user_is_login() == 0:
                 sql = "SELECT short_title, short_description, content, url, ranking, badge, symbol FROM feed "+\
-                "WHERE (asset_class LIKE '%"+x+"%' OR market LIKE '"+x+"') AND type=9 ORDER BY ranking DESC LIMIT 20"
+                "WHERE (asset_class LIKE '%"+x+"%' OR market LIKE '%"+x+"%') AND type=9 ORDER BY ranking DESC LIMIT 12"
             else:
                 sql = "SELECT * FROM (SELECT feed.*, instruments.owner FROM feed JOIN instruments "+\
                 "ON feed.symbol = instruments.symbol WHERE instruments.owner = 10089 AND feed.type=9 ORDER BY feed.globalRank) AS Q1 "+\
                 "UNION "+\
-                "SELECT * FROM (SELECT feed.*, instruments.owner FROM feed JOIN instruments ON feed.symbol = instruments.symbol WHERE feed.globalRank <> 0 AND instruments.y1 > 0 AND (asset_class LIKE '%"+x+"%' OR market LIKE '"+x+"') AND type=9 LIMIT 19) AS Q2"
+                "SELECT * FROM (SELECT feed.*, instruments.owner FROM feed JOIN instruments ON feed.symbol = instruments.symbol WHERE feed.globalRank <> 0 AND instruments.y1 > 0 AND (asset_class LIKE '%"+x+"%' OR market LIKE '%"+x+"%') AND type=9 LIMIT 19) AS Q2"
     except:
         if t == 1:
             sql = "SELECT short_title, short_description, content, url, ranking, badge, symbol FROM feed "+\
             "WHERE type=1 ORDER BY ranking DESC LIMIT 20"
         if t == 9:
-            sql = "SELECT short_title, short_description, content, url, ranking, badge, symbol FROM feed "+\
-            "WHERE type=9 ORDER BY ranking DESC LIMIT 12"
+            if user_is_login() == 0:
+                sql = "SELECT short_title, short_description, content, url, ranking, badge, symbol FROM feed "+\
+                "WHERE type=9 ORDER BY ranking DESC LIMIT 12"
+            else:
+                sql = "SELECT * FROM (SELECT feed.*, instruments.owner FROM feed JOIN instruments "+\
+                "ON feed.symbol = instruments.symbol WHERE instruments.owner = 10089 AND feed.type=9 ORDER BY feed.globalRank) AS Q1 "+\
+                "UNION "+\
+                "SELECT * FROM (SELECT feed.*, instruments.owner FROM feed JOIN instruments ON feed.symbol = instruments.symbol WHERE feed.globalRank <> 0 AND instruments.y1 > 0 AND type=9 LIMIT 19) AS Q2"
+
     try:
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
