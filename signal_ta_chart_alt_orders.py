7fee81d64e4b9130f6336d602ba09e319da1f60e
daivieth <daiviet.huynh@gmail.com>
2019-03-13 11:47:28 +0700

    dev

--- signal_ta_chart_alt_orders.py
+++ signal_ta_chart_alt_orders.py
@@ -333,7 +333,7 @@ def get_sign_ta_chart_alt_orders(uid):
         tech_chart = '' +\
         '        <div class="col-lg-9 col-md-6 col-sm-6 col-xs-12">'+\
         '            <div class="box-part rounded sa-signal-ta-chart">'+\
-        '                  <ul id="sa-tab-sm" class="nav nav-pills" role="tablist">'+\
+        '                  <ul id="sa-tab-sm" class="nav nav-tabs" role="tablist">'+\
         '                    <li class="nav-item">'+\
         '                      <a class="nav-link active" data-toggle="pill" href="'+ tab_1_link +'">'+ tab_1_label +'</a>'+\
         '                    </li>'+\
