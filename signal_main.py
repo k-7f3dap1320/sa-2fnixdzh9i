f61d9fd16bd06bfa370863ba740ca7632df46044
daivieth <daiviet.huynh@gmail.com>
2019-03-13 15:08:10 +0700

    dev

--- signal_main.py
+++ signal_main.py
@@ -22,7 +22,7 @@ from signal_recom_trail_returns import *
 from trades_tab import *
 from font_awesome import *
 from googleanalytics import *
-
+from googleadsense import *
 from sa_db import *
 access_obj = sa_db_access()
 import pymysql.cursors
@@ -42,7 +42,7 @@ def gen_sign_page(uid,appname,burl):
         for row in rs:
             instfullname = row[0]
 
-        r = get_head(  get_loading_head() + get_googleanalytics() + get_title( appname +' - ' + instfullname ) + get_metatags(burl) + get_bootstrap() + get_awesomplete() + get_tablesorter() + get_font_awesome() + get_google_chart_script() + get_stylesheet(burl) )
+        r = get_head(  get_loading_head() + get_googleanalytics() + get_googleadsense() + get_title( appname +' - ' + instfullname ) + get_metatags(burl) + get_bootstrap() + get_awesomplete() + get_tablesorter() + get_font_awesome() + get_google_chart_script() + get_stylesheet(burl) )
         r = r + get_body( get_loading_body(), navbar(burl) + '<div class="box-top"><div class="row">' + get_details_header(uid,burl) + get_sign_header(uid) + get_sign_ta_chart_alt_orders(uid) + get_sign_recommend_trail_returns(uid) + get_trades_box(uid,burl) + '</div></div>' + get_page_footer(burl))
         r = set_page(r)
 
