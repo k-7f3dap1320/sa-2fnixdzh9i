adeeb8d92ffb75a7c10a1e06e93cbc3a856691c5
daivieth <daiviet.huynh@gmail.com>
2019-03-16 19:58:03 +0700

    dev

--- list_instr_n_portf.py
+++ list_instr_n_portf.py
@@ -203,7 +203,7 @@ def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x):
 
 
         l_placeholder = "Type to find for an instrument..."
-        l_caption_to_more_assets = 'Unable find your instrument or symbol? '
+        l_caption_to_more_assets = 'Unable to find your instrument or symbol? More list: '
         l_link_to_more_assets = ''
 
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
