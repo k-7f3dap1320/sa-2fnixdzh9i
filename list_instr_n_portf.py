6fb5cd8c7e197852b53767b8623f2240a0f36b2f
daivieth <daiviet.huynh@gmail.com>
2019-03-16 14:51:53 +0700

    dev

--- list_instr_n_portf.py
+++ list_instr_n_portf.py
@@ -202,11 +202,13 @@ def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x):
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
         sql = "SELECT asset_class_id, asset_class_name FROM asset_class ORDER BY asset_class_name"
+        cr.execute(sql)
+        rs = cr.fetchall()
         for row in rs:
             asset_class_id = row[0]
             asset_class_name = row[1]
             l_link_to_more_assets = l_link_to_more_assets +'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+ burl+'p/?ins=1&step='+ str(step) + '&x='+ asset_class_id +'">'+ asset_class_name +'</a>'
-
+        
         box_content = box_content + '<div class="box">' +\
         '   <div class="row">'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
