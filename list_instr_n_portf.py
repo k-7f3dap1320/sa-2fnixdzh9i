e81b0c780bef37f1eaa5eef12dff1d327fbd3128
daivieth <daiviet.huynh@gmail.com>
2019-03-16 19:40:35 +0700

    dev

--- list_instr_n_portf.py
+++ list_instr_n_portf.py
@@ -201,19 +201,19 @@ def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x):
         if mode == 'portf_select':
             l_caption_to_more_assets = 'Unable find your instrument or symbol? '
             l_link_to_more_assets = ''
+            connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+            cr = connection.cursor(pymysql.cursors.SSCursor)
+            sql = "SELECT asset_class_id, asset_class_name FROM asset_class WHERE asset_class_id <> 'PF:' and asset_class_id <>'MA:' ORDER BY asset_class_name"
+            cr.execute(sql)
+            rs = cr.fetchall()
+            for row in rs:
+                asset_class_id = row[0]
+                asset_class_name = row[1]
+                l_link_to_more_assets = l_link_to_more_assets +'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+ burl+'p/?ins=1&step='+ str(step) + '&x='+ asset_class_id +'">'+ asset_class_name +'</a>'
         else:
             l_caption_to_more_assets = ''
             l_link_to_more_assets = ''
 
-        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-        cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT asset_class_id, asset_class_name FROM asset_class WHERE asset_class_id <> 'PF:' and asset_class_id <>'MA:' ORDER BY asset_class_name"
-        cr.execute(sql)
-        rs = cr.fetchall()
-        for row in rs:
-            asset_class_id = row[0]
-            asset_class_name = row[1]
-            l_link_to_more_assets = l_link_to_more_assets +'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+ burl+'p/?ins=1&step='+ str(step) + '&x='+ asset_class_id +'">'+ asset_class_name +'</a>'
 
         box_content = box_content + '<div class="box">' +\
         '   <div class="row">'+\
