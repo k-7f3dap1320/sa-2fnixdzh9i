60b18533f9a24b119d16c1c9a3093ac04b2904da
daivieth <daiviet.huynh@gmail.com>
2019-03-19 10:40:01 +0700

    dev

--- portf_perf_desc.py
+++ portf_perf_desc.py
@@ -61,18 +61,19 @@ def get_desc_box(uid):
         alloc_fullname = row[1]
         alloc_entry_price = row[2]
         alloc_strategy_order_type = row[3]
-        added_wait_order = False
+        added_order = False
 
         if alloc_order_type.lower() == 'buy' and (alloc_strategy_order_type == 'long/short' or alloc_strategy_order_type == 'long'):
             portf_recomm = portf_recomm  + str(i)+') ' + portf_recomm_buy.replace('{portf_alloc_instr}',alloc_fullname)+ '<br />'
+            added_order = True
         else:
             portf_recomm = portf_recomm  + str(i)+') ' + l_wait + ' ' + alloc_fullname + '<br />'
-            added_wait_order = True
+            added_order = True
 
         if alloc_order_type.lower() == 'sell' and (alloc_strategy_order_type == 'long/short' or alloc_strategy_order_type == 'short'):
             portf_recomm = portf_recomm + str(i)+') ' + portf_recomm_sell.replace('{portf_alloc_instr}',alloc_fullname)+ '<br />'
         else:
-            if added_wait_order == False:
+            if added_order == False:
                 portf_recomm = portf_recomm + str(i)+') ' + l_wait + ' ' + alloc_fullname + '<br />'
 
         i += 1
