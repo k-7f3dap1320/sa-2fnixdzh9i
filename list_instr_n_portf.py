b0aa1c7be6afbf04b1b2700fdc5ccc411e9d955f
daivieth <daiviet.huynh@gmail.com>
2019-03-19 09:52:54 +0700

    dev

--- list_instr_n_portf.py
+++ list_instr_n_portf.py
@@ -341,9 +341,7 @@ def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x):
             else:
                 l_link_to_more_assets = l_link_to_more_assets +'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+ burl+'ls/?w='+ str(what) +'&x='+ asset_class_id +'">'+ asset_class_name +'</a>'
 
-        if mode == 'portf_select':
-            l_link_to_more_assets = l_link_to_more_assets + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+ burl+'p/?ins=1&step='+ str(step) + '&x=">'+ 'Even more...' +'</a>'
-        else:
+        if mode != 'portf_select':
             l_link_to_more_assets = l_link_to_more_assets + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+ burl+'ls/?w='+ str(what) +'&x=">'+ 'Top ranked' +'</a>'
 
 
