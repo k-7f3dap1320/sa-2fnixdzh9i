dc80605aa88c8b8acac5262e0e1ef95a79f205ae
daivieth <daiviet.huynh@gmail.com>
2019-03-21 12:55:40 +0700

    dev

--- app_ogp.py
+++ app_ogp.py
@@ -2,6 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+from sa_func import *
 
 def set_ogp(burl,type):
     try:
@@ -12,7 +13,7 @@ def set_ogp(burl,type):
         '<meta property="og:url" content="http://smartalphatrade.com">'+\
         '<meta property="og:description" content="SmartAlpha will help you to turn any trading portfolio into a profitable one.">'+\
         '<meta property="og:type" content="article">'+\
-        '<meta property="og:image" content="'+ burl +'static/ogp.png">'
+        '<meta property="og:image" content="'+ burl +'static/ogp.png?'+ get_random_num(9) +'">'
 
     except Exception as e: print(e)
     return r
