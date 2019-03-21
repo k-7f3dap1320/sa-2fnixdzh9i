48a9903b8e4bbb7a7c68fcb04eaafe9da823b2e2
daivieth <daiviet.huynh@gmail.com>
2019-03-21 13:08:59 +0700

    dev

--- app_ogp.py
+++ app_ogp.py
@@ -7,13 +7,14 @@ from sa_func import *
 def set_ogp(burl,type):
     try:
 
-        r = '' +\
-        '<meta property="og:title" content="A.I. Powered Trading Insights">'+\
-        '<meta property="og:site_name" content="SmartAlpha Trade">'+\
-        '<meta property="og:url" content="http://smartalphatrade.com">'+\
-        '<meta property="og:description" content="SmartAlpha will help you to turn any trading portfolio into a profitable one.">'+\
-        '<meta property="og:type" content="article">'+\
-        '<meta property="og:image" content="'+ burl +'static/ogp.png?'+ str(get_random_num(9) ) +'">'
+        if int(type) == 1:
+            r = '' +\
+            '<meta property="og:title" content="A.I. Powered Trading Insights">'+\
+            '<meta property="og:site_name" content="SmartAlpha Trade">'+\
+            '<meta property="og:url" content="http://smartalphatrade.com">'+\
+            '<meta property="og:description" content="SmartAlpha will help you to turn any trading portfolio into a profitable one.">'+\
+            '<meta property="og:type" content="article">'+\
+            '<meta property="og:image" content="'+ burl +'static/ogp.png?'+ str(get_random_num(9) ) +'">'
 
     except Exception as e: print(e)
     return r
