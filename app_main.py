b75818fbe7d2605258e215a2054573db209da80b
daivieth <daiviet.huynh@gmail.com>
2019-03-26 15:39:19 +0700

    dev

--- app_main.py
+++ app_main.py
@@ -31,8 +31,6 @@ def gen_main_page(x,appname,burl,is_dashboard):
     else:
         dashboard_content = ''
 
-    print("########" + str(is_dashboard) )
-
     r = get_head( get_loading_head() + get_googleanalytics() + get_googleadsense() + get_title( appname ) + get_metatags(burl) + set_ogp(burl,1,'','') + get_bootstrap() + get_awesomplete() + get_tablesorter() + get_font_awesome() + get_google_chart_script() + get_stylesheet(burl) )
     r = r + get_body( get_loading_body(), navbar(burl) + get_signin_box(burl) + get_box_user_profile_header(burl) + dashboard_content + get_card(x,9,burl) + get_card(x,1,burl) + get_page_footer(burl) )
     r = set_page(r)
