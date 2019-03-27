a88f8519495d5155adc6dbaddef20a31f0415c9d
daivieth <daiviet.huynh@gmail.com>
2019-03-27 13:50:43 +0700

    dev

--- trades_tab.py
+++ trades_tab.py
@@ -86,11 +86,12 @@ def get_trades_tbl(uid,w,burl):
                 "trades.expiration_date, "+\
                 "trades.pnl_pct, "+\
                 "trades.url, "+\
-                "instruments.unit, "+\
+                "a_alloc.unit, "+\
                 "trades.status, "+\
                 "trades.uid "+\
                 "FROM instruments "+\
                 "JOIN portfolios ON instruments.symbol = portfolios.portf_symbol "+\
+                "JOIN instruments as a_alloc ON a_alloc.symbol =  trades.symbol " +\
                 "JOIN trades ON trades.symbol = portfolios.symbol WHERE instruments.owner = " + str(get_user_numeric_id()) + " "+\
                 "AND ((portfolios.strategy_order_type = 'long' AND trades.order_type = 'buy') "+\
                 "OR (portfolios.strategy_order_type = 'short' AND trades.order_type = 'sell') "+\
