be7b14fdf44cdd3e0701dc45b4505c279520fc0f
daivieth <daiviet.huynh@gmail.com>
2019-03-27 18:20:59 +0700

    dev

--- list_instr_n_portf.py
+++ list_instr_n_portf.py
@@ -395,13 +395,14 @@ def get_box_list_instr_n_portf(burl,mode,what,step,portf,maxrow,x):
             else:
                 l_link_to_more_assets = l_link_to_more_assets +'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+ burl+'ls/?w='+ str(what) +'&x='+ asset_class_id +'">'+ asset_class_name +'</a>'
 
+        list_class = 'sa-center-content sa-list-select-100pct sa-instr-n-portf-list'
         search_box = '<div class="input-group input-group-lg"><div class="input-group-prepend"><span class="input-group-text" id="inputGroup-sizing-lg"><i class="fas fa-search" style="font-size: xx-large;"></i></span></div><input type="text" id="filterInput" name="filterInput" onkeyup="filterTable()" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_placeholder +'" autofocus></div>'
-        if mode == 'dashboard': search_box = ''
+        if mode == 'dashboard': search_box = ''; list_class = ''
 
         box_content = box_content + '<div class="box">' +\
         '   <div class="row">'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded sa-center-content sa-list-select-100pct sa-instr-n-portf-list">'+\
+        '            <div class="box-part rounded '+ list_class +'">'+\
         search_box +\
         '            <div>&nbsp;</div>'+\
         gen_instr_n_portf_table(burl,mode,what,step,portf,maxrow,x) +\
