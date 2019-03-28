9e15d990acb18f2edf70bb77fbe9ba46f6311385
daivieth <daiviet.huynh@gmail.com>
2019-03-28 08:01:07 +0700

    dev

--- control_center.py
+++ control_center.py
@@ -27,13 +27,11 @@ def get_control_center(burl):
         l_title_control_center = 'Control Center'
 
         box_content = '<div class="box-top">' +\
-        '   <div class="row">'+\
         '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
         '            <div class="box-part rounded sa-center-content">'+\
         '               <span class="sectiont"><i class="fas fa-tasks"></i>&nbsp;'+ l_title_control_center +'</span>'
         '            </div>'+\
         '        </div>'+\
-        '   </div>'+\
         '</div>'
 
         '''
