ba0583d7a8f75c412a982fd4faca17c3fc47dfd0
daivieth <daiviet.huynh@gmail.com>
2019-03-29 13:27:31 +0700

    dev

--- user_control_center_aggregate_perf.py
+++ user_control_center_aggregate_perf.py
@@ -8,6 +8,7 @@ access_obj = sa_db_access()
 import pymysql.cursors
 import datetime
 import time
+from user_dashboard_count import *
 
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
@@ -42,63 +43,6 @@ def get_aggregate_perf():
     except Exception as e: print(e)
 
     return box_content
-
-def get_num_orders(t):
-    r = 0
-    try:
-
-        query_condition = ''
-        dn = datetime.datetime.now(); dnstr = dn.strftime("%Y%m%d");
-        i = 0
-
-        if t == 'open':
-            query_condition = " "+\
-            "trades.entry_date = "+ str(dnstr) +" AND instruments.owner = '"+ str(get_user_numeric_id() ) +"' AND status = 'active' AND "+\
-            "((portfolios.strategy_order_type = 'long' AND trades.order_type = 'buy') "+\
-            "OR (portfolios.strategy_order_type = 'short' AND trades.order_type = 'sell') "+\
-            "OR (portfolios.strategy_order_type = 'long/short') ) "
-        if t == 'pending':
-            query_condition = " "+\
-            "trades.expiration_date <= "+ str(dnstr) +" AND instruments.owner = '"+ str(get_user_numeric_id() ) +"' AND status = 'active' AND "+\
-            "((portfolios.strategy_order_type = 'long' AND trades.order_type = 'buy') "+\
-            "OR (portfolios.strategy_order_type = 'short' AND trades.order_type = 'sell') "+\
-            "OR (portfolios.strategy_order_type = 'long/short') ) "
-        if t == 'close':
-            query_condition = " "+\
-            "trades.expiration_date = "+ str(dnstr) +" AND instruments.owner = '"+ str(get_user_numeric_id() ) +"' AND status = 'expired' AND "+\
-            "((portfolios.strategy_order_type = 'long' AND trades.order_type = 'buy') "+\
-            "OR (portfolios.strategy_order_type = 'short' AND trades.order_type = 'sell') "+\
-            "OR (portfolios.strategy_order_type = 'long/short') )"
-
-        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-        cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = ' '+\
-        'SELECT '+\
-        'trades.order_type, '+\
-        'trades.fullname, '+\
-        'trades.entry_date, '+\
-        'trades.entry_price, '+\
-        'trades.close_price, '+\
-        'trades.expiration_date, '+\
-        'trades.pnl_pct, '+\
-        'trades.url, '+\
-        'a_alloc.unit, '+\
-        'trades.status, '+\
-        'trades.uid '+\
-        'FROM trades '+\
-        'JOIN portfolios ON portfolios.symbol = trades.symbol '+\
-        'JOIN instruments ON instruments.symbol = portfolios.portf_symbol '+\
-        'JOIN instruments as a_alloc ON a_alloc.symbol = portfolios.symbol '+\
-        'WHERE'+ query_condition
-        cr.execute(sql)
-        rs = cr.fetchall()
-        for row in rs: i +=1
-        r = i
-        cr.close()
-        connection.close()
-
-    except Exception as e: print(e)
-    return r
 
 def get_control_center(burl):
 
