473c032b110aa16c7ebce2425dc86a48485ac023
daivieth <daiviet.huynh@gmail.com>
2019-04-01 12:46:31 +0700

    dev

--- card_chart.py
+++ card_chart.py
@@ -59,7 +59,7 @@ def get_card_chart(uid,color):
     try:
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT price_close FROM chart_data WHERE uid="+ str(uid) + " ORDER BY price_close LIMIT 1"
+        sql = "SELECT MIN(price_close) FROM chart_data WHERE uid="+ str(uid) + " ORDER BY price_close LIMIT 1"
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs:
