7e303dfa95ce12216b56af761981d6748996ffcb
daivieth <daiviet.huynh@gmail.com>
2019-04-01 08:58:14 +0700

    dev

--- user_control_center_aggregate_perf.py
+++ user_control_center_aggregate_perf.py
@@ -16,7 +16,7 @@ def get_current_user_total_account_size():
     try:
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT sum(instruments.account_reference) as total_account FROM instruments WHERE owner = "+ get_user_numeric_id()
+        sql = "SELECT sum(instruments.account_reference) as total_account FROM instruments WHERE owner = "+ str( get_user_numeric_id() )
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: total_account = row[0]
