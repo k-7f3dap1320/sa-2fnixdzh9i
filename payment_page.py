5f7ee1b71435293ef2511c77dc227bbceec40981
daivieth <daiviet.huynh@gmail.com>
2019-04-05 09:39:06 +0700

    dev

--- payment_page.py
+++ payment_page.py
@@ -21,7 +21,7 @@ def get_paypal_payment_button(burl,lang):
         '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">'+\
         '<input type="hidden" name="cmd" value="_s-xclick">'+\
         '<input type="hidden" name="hosted_button_id" value="Q9YFDS96WNT76">'+\
-        '<button type="submit" class="btn btn-lg btn-primary form-signin-btn" type="submit" style="font-size:x-large; font-weight:bolder; width: 80%">'+ l_button_trial +'</button>'+\
+        '<button type="submit" class="btn btn-lg btn-primary form-signin-btn" type="submit" style="font-size:x-large; font-weight:bolder; width: 100%; max-width: 888px;">'+ l_button_trial +'</button>'+\
         '<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">'+\
         '</form>'+\
         '<div>'+ l_then_recurring_monthly +' <i class="fas fa-lock"></i> ('+ l_secure_payment_with_paypal +')</div>'+\
