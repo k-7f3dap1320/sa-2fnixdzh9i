115633d79078d61bd9a14b331b6188fc1d264959
daivieth <daiviet.huynh@gmail.com>
2019-04-07 08:57:48 +0700

    dev

--- portf_gen_user_example.py
+++ portf_gen_user_example.py
@@ -33,14 +33,14 @@ def gen_portf_user_example(burl,acm):
             i = 1
             for row in rs:
                 resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                resp.set_cookie('portf_s_'+str(i)+'_conv', str('neutral'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
                 resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
                 i += 1
             if i < 5:
                 add_additional_asset = "FX:"
                 sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol "+\
                 "WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.y1_signal > 0) AND "+\
-                "(instruments.asset_class LIKE '"+ add_additional_asset +"'') ORDER BY RAND() LIMIT 5"
+                "(instruments.asset_class LIKE '"+ asset_class +"'' OR instruments.asset_class LIKE '"+ add_additional_asset +"'') ORDER BY RAND() LIMIT 5"
                 print(sql)
                 cr.execute(sql)
                 rs = cr.fetchall()
