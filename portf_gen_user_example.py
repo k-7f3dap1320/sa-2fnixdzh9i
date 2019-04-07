bb6874cff21d7098079dd7e5de91abec9abe4519
daivieth <daiviet.huynh@gmail.com>
2019-04-07 10:30:00 +0700

    dev

--- portf_gen_user_example.py
+++ portf_gen_user_example.py
@@ -33,8 +33,8 @@ def gen_portf_user_example(burl,acm):
             i = 1
             for row in rs:
                 resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                resp.set_cookie('portf_s_'+str(i)+'_conv', str('neutral'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                resp.set_cookie('portf_s_'+str(i)+'_type', str('long'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
                 i += 1
             if i < 5:
                 sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol "+\
@@ -45,8 +45,8 @@ def gen_portf_user_example(burl,acm):
                 rs = cr.fetchall()
                 for row in rs:
                     resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                    resp.set_cookie('portf_s_'+str(i)+'_conv', str('neutral'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                    resp.set_cookie('portf_s_'+str(i)+'_type', str('long'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
                     i += 1
             if i < 5:
                 add_additional_asset = "FX:"
