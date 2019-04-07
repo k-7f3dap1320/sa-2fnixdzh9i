3895b827c1c6b406c93c6290cd296caef2bacd1f
daivieth <daiviet.huynh@gmail.com>
2019-04-07 10:32:53 +0700

    dev

--- portf_gen_user_example.py
+++ portf_gen_user_example.py
@@ -26,7 +26,7 @@ def gen_portf_user_example(burl,acm):
         if user_is_login() == 1:
             connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
             cr = connection.cursor(pymysql.cursors.SSCursor)
-            sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.y1_signal > 0 AND instruments.m6_signal > 0 AND instruments.m3_signal > 0 AND instruments.m1_signal > 0) AND (instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') ORDER BY RAND() LIMIT 5"
+            sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.y1_signal > 0 AND instruments.m6_signal > 0 AND instruments.m3_signal > 0 ) AND (instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') ORDER BY RAND() LIMIT 5"
             print(sql)
             cr.execute(sql)
             rs = cr.fetchall()
@@ -58,8 +58,8 @@ def gen_portf_user_example(burl,acm):
                 rs = cr.fetchall()
                 for row in rs:
                     resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                    resp.set_cookie('portf_s_'+str(i)+'_conv', str('neutral'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                    resp.set_cookie('portf_s_'+str(i)+'_type', str('long'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
                     i += 1
 
 
