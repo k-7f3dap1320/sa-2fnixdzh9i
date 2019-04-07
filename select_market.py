4ae1dfb652ee60c608a6dc95efacfd11a064b5a9
daivieth <daiviet.huynh@gmail.com>
2019-04-07 10:12:11 +0700

    dev

--- select_market.py
+++ select_market.py
@@ -26,7 +26,7 @@ import pymysql.cursors
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
 def save_selectmarket(burl,mode,x):
-    r = set_page( get_head('<meta http-equiv="refresh" content="0;URL=' + burl + 'p/?ins=1&step=1" />') + get_body('','') )
+    r = set_page( get_head('<meta http-equiv="refresh" content="0;URL=' + burl + 'genportf/?acm='+ str(x) +'&step=1" />') + get_body('','') )
     try:
         user_id = user_get_uid()
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
@@ -59,7 +59,7 @@ def get_market_list(burl,mode):
             if asset_class_id == 'BD:': label = 'x'
             if asset_class_id == 'CO:': label = 'x'
             if asset_class_id == 'PF:': label = 'x'
-            if not label == 'x': r = r + ' <a href="'+ burl +'genportf/?acm='+ asset_class_id +'&step=1" class="list-group-item list-group-item-action">'+ label +'</a>'
+            if not label == 'x': r = r + ' <a href="'+ burl +'n/?step=d&x='+ asset_class_id +'" class="list-group-item list-group-item-action">'+ label +'</a>'
 
         sql = "SELECT market_id, market_label FROM markets order by market_label"
         cr.execute(sql)
@@ -69,7 +69,7 @@ def get_market_list(burl,mode):
             label = market_label + ' Market'
             #Handle particularities
             if market_id == 'GO>': label = 'x'
-            if not label == 'x': r = r + ' <a href="'+ burl +'genportf/?acm='+ market_id +'&step=1" class="list-group-item list-group-item-action">'+ label +'</a>'
+            if not label == 'x': r = r + ' <a href="'+ burl +'n/?step=d&x='+ market_id +'" class="list-group-item list-group-item-action">'+ label +'</a>'
 
         r = r + '</div>'
 
