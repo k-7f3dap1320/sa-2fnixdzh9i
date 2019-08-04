0c17fd430f463480824454544d78aef7e5df84d4
The Cat Tamer <dh@taatu.co>
2019-08-04 19:32:10 +0700

    dev

--- signal_main.py
+++ signal_main.py
@@ -31,10 +31,24 @@ import pymysql.cursors
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
+def get_uid_from_tvs(tvws):
+    r = 0
+    try:
+        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT uid FROM symbol_list WHERE tradingview ='"+ str(tvws) +"' "
+        cr.execute(sql)
+        rs = cr.fetchall()
+        for row in rs: r = row[0]
+        cr.close()
+        connection.close()
+    except Exception as e: print(e)
+    return r
+
 def gen_sign_page(uid,tvws,appname,burl):
 
     try:
-        if tvws is not None: uid = tvws
+        if tvws is not None: uid = get_uid_from_tvs(tvws)
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
         sql = "SELECT instruments.fullname FROM `symbol_list` JOIN instruments ON symbol_list.symbol = instruments.symbol "+\
