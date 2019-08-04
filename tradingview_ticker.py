0dd47da1620e2d541dfa20dcb6977bb5df9fe4d4
The Cat Tamer <dh@taatu.co>
2019-08-04 17:06:37 +0700

    dev

--- tradingview_ticker.py
+++ tradingview_ticker.py
@@ -20,7 +20,7 @@ def get_tradingview_ticker(uid):
         "FROM instruments "+\
         "JOIN portfolios ON instruments.symbol = portfolios.portf_symbol "+\
         "JOIN symbol_list ON portfolios.symbol = symbol_list.symbol "+\
-        "WHERE owner='"+ str(uid) +"' "+\
+        "WHERE owner='"+ str(uid) +"' "
         print(sql)
         cr.execute(sql)
         rs = cr.fetchall()
