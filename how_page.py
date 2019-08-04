9901be403c1f72a9f15738c701a34f05193203a8
The Cat Tamer <dh@taatu.co>
2019-08-04 21:05:54 +0700

    dev

--- how_page.py
+++ how_page.py
@@ -14,7 +14,17 @@ def get_help_content(burl):
     box_content = ''
 
     try:
-        pass
+
+        box_content = '<div class="box-top">' +\
+        '   <div class="row">'+\
+        '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
+        '            <div class="box-part rounded sa-center-content">'+\
+        'Content'+\
+        '            </div>'+\
+        '        </div>'+\
+        '   </div>'+\
+        '</div>'
+
 
     except Exception as e: print(e)
 
