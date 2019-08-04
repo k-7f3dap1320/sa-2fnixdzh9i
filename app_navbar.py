72eaa1e7bf7afd9013b767bc2fea6a0192c08d5b
The Cat Tamer <dh@taatu.co>
2019-08-04 21:30:57 +0700

    dev

--- app_navbar.py
+++ app_navbar.py
@@ -150,7 +150,7 @@ def navbar(burl):
         '    </li>' +\
         get_portfolio_button(burl)
     else:
-        rightsidemenu = get_how_menu(burl) + '<li class="nav-item"><a href="'+burl+'pricing" class="btn btn-sm btn-danger btn-block form-signin-btn"><i class="fas fa-sign-in-alt"></i>&nbsp;'+ l_join_now_btn +'</a></li>'
+        rightsidemenu = '<strong>'+ get_how_menu(burl) + '</strong>' +'<li class="nav-item"><a href="'+burl+'pricing" class="btn btn-sm btn-danger btn-block form-signin-btn"><i class="fas fa-sign-in-alt"></i>&nbsp;'+ l_join_now_btn +'</a></li>'
 
     r = ''+\
     '<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">'+\
