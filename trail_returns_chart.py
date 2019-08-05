490115e07a795e55ecf8bedbd2942ac39b007b80
The Cat Tamer <dh@taatu.co>
2019-08-05 12:46:42 +0700

    dev

--- trail_returns_chart.py
+++ trail_returns_chart.py
@@ -138,7 +138,7 @@ def get_trailing_returns(uid):
     "       fontSize: "+ str( fontSize ) + "," +\
     "      	legend: 'top'," +\
     "        title: ''," +\
-    "        chartArea: {width: '100%'}," +\
+    "        chartArea: {width: '50%'}," +\
     "        annotations: {" +\
     "          alwaysOutside: true," +\
     "          textStyle: {" +\
@@ -157,7 +157,7 @@ def get_trailing_returns(uid):
     "          }" +\
     "        }," +\
     "        series: {0:{color:'#497f8c'}, 1:{color: '#c9d6ea'} }," +\
-    "        chartArea: {width:'60%',height:'80%'}," +\
+    "        chartArea: {height:'80%'}," +\
     "        hAxis: {" +\
     "          title: '" + l_as_date + "' " +\
     "        }," +\
