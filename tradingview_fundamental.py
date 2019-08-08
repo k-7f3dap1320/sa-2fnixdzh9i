4513b45bf29b15fb0f7098843ff072050bf6d3d4
The Cat Tamer <dh@taatu.co>
2019-08-08 17:22:11 +0700

    dev

--- tradingview_fundamental.py
+++ tradingview_fundamental.py
@@ -11,7 +11,7 @@ from googleanalytics import *; from tablesorter import *
 
 
 def get_tradingview_fundamental_widget(uid):
-    r = '<iframe src="fd/?uid='+ str(uid) +'" frameborder="0" width="100%" height="100%">'
+    r = '<iframe src="../fd/?uid='+ str(uid) +'" frameborder="0" width="100%" height="100%">'
     return r
 
 def get_tradingview_fundamental(uid):
