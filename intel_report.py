133df8e5ed8813f20553d70c8c2bd11446a70188
The Cat Tamer <dh@taatu.co>
2019-08-15 20:27:17 +0700

    dev

--- intel_report.py
+++ intel_report.py
@@ -53,7 +53,7 @@ def get_intel_content(burl):
     try:
 
         box_content = ''+\
-        '<div class="box-top">'
+        '<div class="box-top">'+\
         '<div class="row">' +\
         '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"></div>'+\
         '    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12"><div class="box-part rounded">'+ get_report_title() +'</div></div>'+\
