a9ce1555ad464fa19f7b053b006d90ef3e6766c0
The Cat Tamer <dh@taatu.co>
2019-08-15 14:12:45 +0700

    dev

--- intel_report.py
+++ intel_report.py
@@ -58,7 +58,7 @@ def get_signals_lines(burl):
             "OR (portfolios.strategy_order_type = 'short' AND trades.order_type = 'sell') "+\
             "OR (portfolios.strategy_order_type = 'long/short') ) AND "+\
             "((trades.entry_date = " + dnstr + " AND instruments.owner = " + str(get_user_numeric_id()) + " AND status = 'active') OR "+\
-            "(trades.expiration_date <= " + dnstr + " AND instruments.owner = "+ str(get_user_numeric_id()) +" AND status = 'active')"
+            "(trades.expiration_date <= " + dnstr + " AND instruments.owner = "+ str(get_user_numeric_id()) +" AND status = 'active') )"
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs:
