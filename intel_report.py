207ac3e94d8495a0d975459c0bc508c214b9d6e1
The Cat Tamer <dh@taatu.co>
2019-08-15 19:34:23 +0700

    dev

--- intel_report.py
+++ intel_report.py
@@ -57,7 +57,11 @@ def get_intel_content(burl):
         '<div class="row">' +\
         '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><div class="box-part rounded"></div></div>'+\
         '    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12"><div class="box-part rounded">'+ get_report_title() +'</div></div>'+\
+        '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><div class="box-part rounded"></div></div>'+\
+        '</div>'+\
         get_signals_lines(burl) +\
+        '<div class="row">' +\
+        '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><div class="box-part rounded"></div></div>'+\
         '    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12"><div class="box-part rounded"></div></div>'+\
         '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><div class="box-part rounded"></div></div>'+\
         '</div>'
@@ -91,9 +95,13 @@ def get_signals_lines(burl):
         for row in rs:
             uid = row[0]
             content = content +\
+            '<div class="row">' +\
+            '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><div class="box-part rounded"></div></div>'+\
             '    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><div class="box-part rounded sa-center-content">'+ get_tradingview_mini_chart(uid,'100%','200','false','1m') +'</div></div>'+\
             '    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="box-part rounded sa-center-content">'+ get_signal_details(uid) + '<span style="font-size: small">'+ get_recomm(uid)  +'</span></div></div>'+\
-            '    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12"><div class="box-part rounded sa-center-content"><hr /></div></div>'
+            '    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12"><div class="box-part rounded sa-center-content"><hr /></div></div>'+\
+            '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><div class="box-part rounded"></div></div>'+\
+            '</div>'
         cr.close()
         connection.close()
 
