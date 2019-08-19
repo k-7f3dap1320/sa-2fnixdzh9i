dafdd0eb0af3f7c4ee8c307cdb1e5ebe0dbf2599
The Cat Tamer <dh@taatu.co>
2019-08-19 23:44:57 +0700

    dev

--- payment_page.py
+++ payment_page.py
@@ -70,7 +70,7 @@ def get_box_plan_selection(burl):
         '  <thead>'+\
         '    <tr>'+\
         '      <th scope="col" style="vertical-align: top;">&nbsp;</th>'+\
-        '      <th scope="col" style="vertical-align: top;"><h4>SmartAlpha Pro</h4></th>'+\
+        '      <th scope="col" style="vertical-align: top;"><h4>SmartAlpha Trading Intelligence</h4></th>'+\
         '    </tr>'+\
         '    <tr>'+\
         '      <th scope="col" style="vertical-align: top;">Features</th>'+\
