75932c409b82b55c96bb1807a4e23cf668085e22
The Cat Tamer <dh@taatu.co>
2019-08-26 20:09:21 +0700

    dev

--- signal_ta_chart_alt_orders.py
+++ signal_ta_chart_alt_orders.py
@@ -185,7 +185,7 @@ def get_ta_chart(uid):
     '          vAxis: { viewWindow:{min: '+ str( minval ) +', viewWindowMode: "explicit"}, gridlines: { color: "transparent" } },'+\
     '          hAxis: { gridlines: { count: 4, color: "transparent" } }, '+\
     '          series:{'+\
-    '                   0: {areaOpacity: 0.1, color: "#17a2b8", lineWidth: 4},'+\
+    '                   0: {areaOpacity: 0.3, color: "#17a2b8", lineWidth: 2},'+\
     '                   1: {areaOpacity: 0.2, color: "#ff9800", lineWidth: 3},'+\
     '                   2: {areaOpacity: 0, color: "gray", lineWidth: 2},'+\
     '                   3: {areaOpacity: 0, color: "gray", lineWidth: 2},'+\
