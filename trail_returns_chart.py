2dd5df44f28f31f18f03ad6b96dc353bf66f2483
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-08-27 11:57:33 +0700

    dev

--- trail_returns_chart.py
+++ trail_returns_chart.py
@@ -2,6 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+from app_cookie import *
 from sa_db import *
 access_obj = sa_db_access()
 import pymysql.cursors
@@ -200,7 +201,7 @@ def get_trailing_returns(uid):
     "            }" +\
     "          }" +\
     "        }," +\
-    "        series: {0:{color:'blue'}, 1:{color: '#c9d6ea'} }," +\
+    "        series: {0:{color: "+ theme_return_this("'blue'","'orange'") +", 1:{color: '#c9d6ea'} }," +\
     "        chartArea: {width:'80%',height:'80%'}," +\
     "        hAxis: {" +\
     "          title: '" + l_as_date + "', " +\
