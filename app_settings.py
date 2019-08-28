11e050da920f654ad010f738b77d6909f3942a0f
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-08-28 16:54:58 +0700

    dev

--- app_settings.py
+++ app_settings.py
@@ -21,7 +21,7 @@ def get_settings_content(burl):
         '   <div class="row">'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
         '            <div class="box-part rounded sa-center-content" style+"'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
-        'Content goes here...'+\
+        'Settings...'+\
         '            </div>'+\
         '        </div>'+\
         '   </div>'+\
