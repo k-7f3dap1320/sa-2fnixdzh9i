a31f6f3656fd661dee47f58b081d3a24c7e09062
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-08-28 09:36:47 +0700

    dev

--- signal_main.py
+++ signal_main.py
@@ -51,7 +51,7 @@ def get_sign_header(uid):
     try:
         content = ''+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded">'+\
+        '            <div class="box-part rounded" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
         get_signal_details(uid) +\
         '            </div>'+\
         '        </div>'
--- signal_recom_trail_returns.py
+++ signal_recom_trail_returns.py
@@ -2,6 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+from app_cookie import *
 from sa_db import *
 access_obj = sa_db_access()
 import pymysql.cursors
@@ -34,7 +35,7 @@ def get_recomm_layout(uid):
         l_title = 'Technical Recommendation'
         content = '' +\
         '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded sa-signal-recomm-trail-ret">'+\
+        '            <div class="box-part rounded sa-signal-recomm-trail-ret" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
         '               <div><h6>'+ l_title +'</h6></div>'+\
         '               <div>'+ get_recomm(uid) +'</div>'+\
         '            </div>'+\
@@ -60,7 +61,7 @@ def get_chart_box(uid):
 
     r = '' +\
     '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
-    '            <div class="box-part rounded sa-signal-recomm-trail-ret">'+\
+    '            <div class="box-part rounded sa-signal-recomm-trail-ret" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
     '               <div><h6>'+ l_title +'</h6></div>'+\
     get_trailing_returns(uid) +\
     '            </div>'+\
