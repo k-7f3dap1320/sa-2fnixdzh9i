64a69cc42351632c9a0de8bf1f3e787374b39f1f
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-08-28 10:31:22 +0700

    dev

--- portf_risk_trail_returns.py
+++ portf_risk_trail_returns.py
@@ -153,7 +153,7 @@ def get_box_risk_content(uid):
 
         box_content = '' +\
         '        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded sa-signal-recomm-trail-ret">'+\
+        '            <div class="box-part rounded sa-signal-recomm-trail-ret" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
         '               <div><h6>'+ l_title +'</h6></div>'+\
         '                   <div>'+ text_content +'</div>'+\
         '<div>'+ get_risk_table(uid) +'</div>'+\
