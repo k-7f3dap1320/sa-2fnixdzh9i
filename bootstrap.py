4f8c73a79fa5e428870f4bbcffc754bc943ffd19
The Cat Tamer <dh@taatu.co>
2019-08-28 07:21:59 +0700

    dev

--- bootstrap.py
+++ bootstrap.py
@@ -11,12 +11,12 @@ def get_bootstrap(theme,burl):
     bootcss_dark_url =burl + 'static/bootstrap_custom_dark.css?' + get_random_str(10)
     selected_bootcss = ''
 
-    if theme == 'dark':
-        selected_bootcss = bootcss_dark_url
-        selected_bootcss_integrity = ''
-    else:
+    if theme == 'light':
         selected_bootcss = bootcss_url
         selected_bootcss_integrity = bootcss_integrity
+    else:
+        selected_bootcss = bootcss_dark_url
+        selected_bootcss_integrity = ''
 
     jquery = '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>'
     popper = '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>'
