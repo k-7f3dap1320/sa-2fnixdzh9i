7696f97fca82871b9c5bbf71719d4fc75e2bfba4
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-08-29 15:22:17 +0700

    dev

--- gcharts_columnchart.py
+++ gcharts_columnchart.py
@@ -24,7 +24,7 @@ def get_gcharts_column(chart_id,data,data_label,data_color,data_annotation,title
         '        bar: {groupWidth: "90%"},'+\
         '        legend: { position: "'+ str(legend_position) +'" },'+\
         '        vAxis: { gridlines: {color: "transparent"}},'+\
-        '        hAxis: { gridlines: {color: "transparent"}},'+\      
+        '        hAxis: { gridlines: {color: "transparent"}},'+\
         '      };'+\
         '      var chart = new google.visualization.ColumnChart(document.getElementById("'+ str(chart_id) +'"));'+\
         '      chart.draw(view, options);'+\
