8521dd785c03451363ddb7ae9103a7a1dc3dc2c1
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-08-30 10:18:31 +0700

    dev

--- app_metatags.py
+++ app_metatags.py
@@ -6,10 +6,12 @@ from app_stylesheet import *
 from sa_func import *
 
 def get_metatags(burl):
+    charset = '<meta charset="UTF-8" />'
     theme_color = '<meta name="theme-color" content="'+ get_theme_color() +'" />'
     favicon = '<link rel="icon" href="'+ burl +'static/favicon.ico?r='+ get_random_str(9) +'">'
 
-    r = theme_color +\
+    r = charset +\
+        theme_color +\
         favicon
 
     return r
