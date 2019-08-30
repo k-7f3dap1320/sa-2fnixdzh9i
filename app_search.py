f111b3857e16a23d2ebd01a5a7eb8623417b607e
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-08-30 17:34:42 +0700

    dev

--- app_search.py
+++ app_search.py
@@ -142,7 +142,7 @@ def get_search_page(appname,burl):
     r = ''
     try:
         r = get_head( get_loading_head() + get_googleanalytics() + get_title( appname ) + get_metatags(burl) + set_ogp(burl,1,'','') + get_bootstrap( get_sa_theme(),burl ) + get_awesomplete() + get_tablesorter() + get_font_awesome() + get_stylesheet(burl) )
-        r = r + get_body( get_loading_body(), navbar(burl) + get_search_page_content(burl) + get_page_footer(burl) )
+        r = r + get_body( get_loading_body(),  get_search_page_content(burl) )
         r = set_page(r)
     except Exception as e: print(e)
 
