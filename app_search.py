3f01fb1ce26f07fed6249224e4cc82967fecec53
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-03 18:03:49 +0700

    dev

--- app_search.py
+++ app_search.py
@@ -164,7 +164,7 @@ def get_search_page(appname,burl,nonavbar):
     r = ''
     try:
         navbar = ''
-        if nonavbar is None or nonavbar == 0:
+        if nonavbar is None:
             navbar = navbar(burl,1)
 
         r = get_head( get_loading_head() + get_googleanalytics() + get_title( appname ) + get_metatags(burl) + set_ogp(burl,1,'','') + get_bootstrap( get_sa_theme(),burl ) + get_tablesorter() + get_font_awesome() + get_stylesheet(burl) )
