020f1aa46ed47982bacb72edd8fa3fa6a8ee86bb
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-03 13:40:17 +0700

    dev

--- tradingview_indicators.py
+++ tradingview_indicators.py
@@ -20,7 +20,7 @@ def get_tradingview_indicators(suid,width,height):
 
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT tradingview FROM symbol_list WHERE uid ='"+ str(suid) +"'"
+        sql = "SELECT tradingview_ta FROM symbol_list WHERE uid ='"+ str(suid) +"'"
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: symbol = row[0]
--- tradingview_profile.py
+++ tradingview_profile.py
@@ -24,7 +24,7 @@ def get_tradingview_profile(uid):
 
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT tradingview FROM symbol_list WHERE uid ='"+ str(uid) +"'"
+        sql = "SELECT tradingview_profile FROM symbol_list WHERE uid ='"+ str(uid) +"'"
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: symbol = row[0]
