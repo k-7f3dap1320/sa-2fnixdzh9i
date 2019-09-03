d607629f51271c0612d1d77e06a46f0d461b86f0
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-03 13:20:29 +0700

    dev

--- tradingview_chart.py
+++ tradingview_chart.py
@@ -19,7 +19,7 @@ def get_tradingview_chart(suid,width,height):
 
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT tradingview FROM symbol_list WHERE uid ='"+ str(suid) +"'"
+        sql = "SELECT tradingview_chart FROM symbol_list WHERE uid ='"+ str(suid) +"'"
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: symbol = row[0]
