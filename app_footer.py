84e144de3bf8a8af1d2524ed0954e3625e66bfe2
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-09 10:44:22 +0700

    dev

--- app_footer.py
+++ app_footer.py
@@ -2,6 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+from app_cookie import *
 
 def get_page_footer(burl):
 
@@ -9,58 +10,59 @@ def get_page_footer(burl):
 
     try:
 
-        box_content = ' '+\
-        '<div class="box">' +\
-        '   <div class="row">'+\
-        '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
-        '            <div class="sa-center-content footer">'+\
-        '            </div>'+\
-        '        </div>'+\
-        '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
-        '            <div class="sa-center-content footer">'+\
-        '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html" target="_blank" class="text-info">Company</a></span>' +\
-        '               <br /><br /></strong>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
-        '            <div class="sa-center-content footer">'+\
-        '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank" class="text-info">Contact Us</a></span>' +\
-        '               <br /><br /></strong>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
-        '            <div class="sa-center-content footer">'+\
-        '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#business" target="_blank" class="text-info">SmartAlpha for Business</a></span>' +\
-        '               <br /><br /></strong>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
-        '            <div class="sa-center-content footer">'+\
-        '               <br /><br /><strong>'+\
-        '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#tc" target="_blank" class="text-info">Terms and Conditions</a></span>' +\
-        '               <br /><br /></strong>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '   </div>'+\
-        '   <div class="row">'+\
-        '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
-        '            <div class="sa-center-content footer">'+\
-        '            </div>'+\
-        '        </div>'+\
-        '   </div>'+\
-        '</div>'+\
-        '<hr />'+\
-        '<div class="box">'+\
-        '   <div class="row">'+\
-        '      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
-        '           <div class="sa-center-content text-secondary">Made with <i class="fas fa-heart"></i> from London, Nairobi to Bangkok - <span class="text-muted">Copyright &copy; Taatu Ltd. 2019</span></div>'+\
-        '      </div>'+\
-        '   </div>'+\
-        '</div>'+\
-        '<br /><br /><br /><br />'
+        if user_is_login() == 0:
+            box_content = ' '+\
+            '<div class="box">' +\
+            '   <div class="row">'+\
+            '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
+            '            <div class="sa-center-content footer">'+\
+            '            </div>'+\
+            '        </div>'+\
+            '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
+            '            <div class="sa-center-content footer">'+\
+            '               <br /><br /><strong>'+\
+            '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html" target="_blank" class="text-info">Company</a></span>' +\
+            '               <br /><br /></strong>'+\
+            '            </div>'+\
+            '        </div>'+\
+            '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
+            '            <div class="sa-center-content footer">'+\
+            '               <br /><br /><strong>'+\
+            '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#contact" target="_blank" class="text-info">Contact Us</a></span>' +\
+            '               <br /><br /></strong>'+\
+            '            </div>'+\
+            '        </div>'+\
+            '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
+            '            <div class="sa-center-content footer">'+\
+            '               <br /><br /><strong>'+\
+            '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#business" target="_blank" class="text-info">SmartAlpha for Business</a></span>' +\
+            '               <br /><br /></strong>'+\
+            '            </div>'+\
+            '        </div>'+\
+            '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
+            '            <div class="sa-center-content footer">'+\
+            '               <br /><br /><strong>'+\
+            '               <span style="font-size:large;"><a href="'+ burl +'static/taatu/index.html#tc" target="_blank" class="text-info">Terms and Conditions</a></span>' +\
+            '               <br /><br /></strong>'+\
+            '            </div>'+\
+            '        </div>'+\
+            '   </div>'+\
+            '   <div class="row">'+\
+            '        <div class="col-lg-2 col-md-2 col-sm-1 col-xs-1">'+\
+            '            <div class="sa-center-content footer">'+\
+            '            </div>'+\
+            '        </div>'+\
+            '   </div>'+\
+            '</div>'+\
+            '<hr />'+\
+            '<div class="box">'+\
+            '   <div class="row">'+\
+            '      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
+            '           <div class="sa-center-content text-secondary">Made with <i class="fas fa-heart"></i> from London, Nairobi to Bangkok - <span class="text-muted">Copyright &copy; Taatu Ltd. 2019</span></div>'+\
+            '      </div>'+\
+            '   </div>'+\
+            '</div>'+\
+            '<br /><br /><br /><br />'
 
 
     except Exception as e: print(e)
