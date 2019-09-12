06cacabda2db5f00837ba4be065f38eb1a0a7eaf
The Cat Tamer <dh@taatu.co>
2019-09-12 11:24:58 +0700

    dev

--- app_search.py
+++ app_search.py
@@ -33,7 +33,7 @@ def get_search_table_content(burl,nonavbar):
 
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = 'SELECT feed.search, feed.content, markets.market_label, feed.url, feed.type FROM feed LEFT JOIN markets ON markets.market_id = feed.market WHERE feed.type<>9 ORDER BY feed.symbol'
+        sql = 'SELECT feed.search, feed.content, markets.market_label, feed.url, feed.type FROM feed LEFT JOIN markets ON markets.market_id = feed.market WHERE (feed.type<>9 AND feed.type<>3) ORDER BY feed.symbol'
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs:
