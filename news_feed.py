9e1daf323b8471eb819a482bd2a75e7c9290d98d
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-18 12:22:21 +0700

    dev

--- news_feed.py
+++ news_feed.py
@@ -47,7 +47,7 @@ def get_newsfeed(x,suid,numline,show_chart):
             newsrow = newsrow +\
             '<div class="row">'+\
             '    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 d-none d-md-block"></div>'+\
-            '    <div class="'+ bsclass +' col-sm-12 col-xs-12" style="border-top:solid; border-left:none; border-right:none; border-bottom:none; border-width: 0.5px; border-style: dotted;"> '+\
+            '    <div class="'+ bsclass +' col-sm-12 col-xs-12" style="border-top:0.5px; border-top-style: dotted;"> '+\
             '       <strong>'+ str(i) +')'+'</strong>'+\
             '       <strong><a data-toggle="collapse" href="#'+ str(unistr)+'">'+ news_title +'</a></strong>'+\
             '       <div class="collapse" id="'+ str(unistr) +'">'+ news_content +'<br /><br /></div>'+\
