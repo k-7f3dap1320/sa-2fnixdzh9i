5852a5da80f9fa34579101c81c786b3eedba6212
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-18 11:31:41 +0700

    dev

--- news_feed.py
+++ news_feed.py
@@ -45,18 +45,16 @@ def get_newsfeed(x,suid,numline,show_chart):
             news_content = str(row[1]) +' </ br></ br>'+ '<a href="'+ str(row[2]) +'" target="_blank">'+ l_view_article +'</a>'
 
             newsrow = newsrow +\
-            '<hr />'+\
             '<div class="row">'+\
             '    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 d-none d-md-block"></div>'+\
             '    <div class="'+ bsclass +' col-sm-12 col-xs-12"> '+\
             '       <strong>'+ str(i) +')'+'</strong>&nbsp;'+\
             '       <strong><a data-toggle="collapse" href="#'+ str(unistr)+'">'+ news_title +'</a></strong>'+\
-            '       <div class="collapse" id="'+ str(unistr) +'"><br /><br />'+ news_content +'<br /><br /></div>'+\
+            '       <div class="collapse" id="'+ str(unistr) +'"><br />'+ news_content +'<br /></div>'+\
             '    </div>'+\
             '    <div class="'+ bsclass +' col-sm-1 col-xs-1 d-sm-block"></div>'+\
             '    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 d-none d-md-block"></div>'+\
             '</div>'+\
-            '<hr />'
             i += 1
 
         cr.close()
