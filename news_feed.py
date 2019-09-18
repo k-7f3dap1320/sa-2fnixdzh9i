673d225acc53c26266977ad6edb07436d917c9ec
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-18 14:28:17 +0700

    dev

--- news_feed.py
+++ news_feed.py
@@ -34,6 +34,7 @@ def get_newsfeed(x,suid,numline,show_chart):
         'asset_class LIKE "%%" '+\
         'AND '+\
         'lang LIKE "%'+ str(lang) +'%" '+\
+        'AND ABS(ranking)>=0.4 '+\
         'ORDER BY date DESC LIMIT '+ str(numline)
         cr.execute(sql)
         rs = cr.fetchall()
@@ -52,12 +53,12 @@ def get_newsfeed(x,suid,numline,show_chart):
             newsrow = newsrow +\
             '<div class="row">'+\
             '    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 d-none d-lg-block"></div>'+\
-            '    <div class="'+ bsclass +' col-sm-12 col-xs-12" style="border-top:0.5px; border-top-style: dotted; background-color:'+ rowbgcolor +'"> '+\
+            '    <div class="'+ bsclass +' col-sm-6 col-xs-12" style="border-top:0.5px; border-top-style: dotted; background-color:'+ rowbgcolor +'"> '+\
             '       <strong>'+'<i class="fas fa-rss-square"></i>'+'</strong>'+\
             '       <strong><a data-toggle="collapse" href="#'+ str(unistr)+'">'+ news_title +'</a></strong>'+\
             '       <div class="collapse" id="'+ str(unistr) +'">'+ news_content +'<br /><br /></div>'+\
             '    </div>'+\
-            '    <div class="'+ bsclass +' col-sm-1 col-xs-1 d-sm-block" style="border-top:0.5px; border-top-style: dotted; background-color:'+ rowbgcolor +'"></div>'+\
+            '    <div class="'+ bsclass +' col-sm-6 col-xs-1 d-sm-block" style="border-top:0.5px; border-top-style: dotted; background-color:'+ rowbgcolor +'"></div>'+\
             '    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 d-none d-lg-block"></div>'+\
             '</div>'
             i += 1
