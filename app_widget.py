469efd1c85b64670998e8da945b02aff22d48b31
The Cat Tamer <dh@taatu.co>
2019-09-19 19:20:29 +0700

    dev

--- app_widget.py
+++ app_widget.py
@@ -31,9 +31,9 @@ def get_widget_content(burl,nonavbar,funcname):
             box_class ='box-top'
             box_vh = '89vh'
 
-        box_content = '<div class="'+ box_class +'">' +\
-        '   <div class="row">'+\
-        '        <div style="height: '+ box_vh +'; width: '+ box_vw +';">'+\
+        box_content = ' '+\
+        '<div class="'+ box_class +'">' +\
+        '   <div style="height: '+ box_vh +'; width: '+ box_vw +';">'+\
         eval(funcname)+\
         '   </div>'+\
         '</div>'
