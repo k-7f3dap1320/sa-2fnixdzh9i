ad12dfe09c16eed79a19812924b520df36244c01
The Cat Tamer <dh@taatu.co>
2019-09-21 16:24:33 +0700

    d-Sat 09/21/2019-16:24:32.93

--- setup.bat
+++ setup.bat
@@ -1,9 +1,17 @@
 REM Edt Configuration here #####################################################
 SET PY_VER=Python37-32
 REM ############################################################################
+@ECHO.
+@ECHO.
+@ECHO --------------------------------------------------------------------------
 @ECHO Due to sometimes Windows Environment Variables are not set properly
 @ECHO For Python, pip etc... this is why it is recommended to run pip:
 @ECHO invoking python.exe (python -m pip install requests) for instance
+@ECHO --------------------------------------------------------------------------
+@ECHO Requirements before starting the process...
+@ECHO Make sure to have the AWS access Key and the security key
+@ECHO --------------------------------------------------------------------------
+pause
 
 SET APP_DIR=%~dp0
 SET _PY_EXE="%LOCALAPPDATA%\Programs\Python\%PY_VER%\python.exe"
@@ -19,8 +27,9 @@ git add .
 git commit -m "d-%date%-%time%"
 
 %_VIRTUALENV% flask-prod
-@ECHO ###########################
+@ECHO --------------------------------------------------------------------------
 @ECHO Type "deploy" to deploy to AWS Elastic Beanstalk
-@ECHO ###########################
+@ECHO --------------------------------------------------------------------------
+@ECHO OFF
 
 %_ACTIVATE%
