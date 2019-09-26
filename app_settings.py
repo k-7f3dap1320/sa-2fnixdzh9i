77116b8520287cea12f7da7a841a8bec27e585f0
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-26 17:29:19 +0700

    dev

--- app_settings.py
+++ app_settings.py
@@ -20,83 +20,105 @@ db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access
 def get_settings_content(burl,step):
     box_content = ''
     try:
-        l_profile_section = 'Profile Settings'
-        l_fullname = 'Fullname'
-        l_nickname = 'Displayed Nickname'
-        l_email = 'Email address'
-        l_password = 'Password and Security'
-        l_market_ac = 'Default asset class / market to show in your newsfeed'
-        l_password_label = 'Password'
-        l_password_btn = 'Change Password'
-        l_save_btn = 'Save changes'
-        l_cancel_link = 'Cancel'
+        if user_is_login() == 1:
+            l_profile_section = 'Profile Settings'
+            l_fullname = 'Fullname'
+            l_nickname = 'Displayed Nickname'
+            l_email = 'Email address'
+            l_password = 'Password and Security'
+            l_market_ac = 'Default asset class / market to show in your newsfeed'
+            l_password_label = 'Password'
+            l_password_btn = 'Change Password'
+            l_save_btn = 'Save changes'
+            l_cancel_link = 'Cancel'
 
-        box_content = ' '+\
-        '<div class="box-top">' +\
-        '   <div class="row">'+\
-        '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
-        '               <form name="settingsForm" id="settingsForm" method="post" action="'+ burl +'settings/?step=2" >'+\
-        '                   <span class="sectiont"><i class="fas fa-sliders-h"></i>&nbsp;'+ l_profile_section +'</span><div style="height: 30px;"></div>'+\
-        '                   <!--------------------- Name --------------------->'+\
-        '                   <div class="input-group" style="max-width:888px">'+\
-        '                       <span class="text-primary" style="width:200px;">'+ l_fullname +'</span><span>&nbsp;&nbsp;</span>'+\
-        '                       <div class="input-group-prepend">'+\
-        '                           <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-user-alt" style="font-size: large;"></i></span>'+\
-        '                       </div>'+\
-        '                           <input type="text" id="name" name="name" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_fullname +'" required autofocus>'+\
-        '                   </div>'+\
-        '                   <!--------------------- Nickname ----------------->'+\
-        '                   <div class="input-group" style="max-width:888px">'+\
-        '                       <span class="text-primary" style="width:200px;">'+ l_nickname +'</span><span>&nbsp;&nbsp;</span>'+\
-        '                       <div class="input-group-prepend">'+\
-        '                           <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-user-alt" style="font-size: large;"></i></span>'+\
-        '                       </div>'+\
-        '                           <input type="text" id="name" name="name" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_nickname +'" required autofocus>'+\
-        '                   </div>'+\
-        '                   <!--------------------- Email -------------------->'+\
-        '                   <div class="input-group" style="max-width:888px">'+\
-        '                       <span class="text-primary" style="width:200px;">'+ l_email +'</span><span>&nbsp;&nbsp;</span>'+\
-        '                       <div class="input-group-prepend">'+\
-        '                           <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-at" style="font-size: large;"></i></span>'+\
-        '                       </div>'+\
-        '                           <input type="email" id="email" name="email" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_email +'" required autofocus>'+\
-        '                   </div>'+\
-        '                   <div style="height: 30px;"></div>'+\
-        '                   <!---------------- Trader: Market ---------------->'+\
-        '                   <div>'+\
-        '                       <span class="text-primary">'+ l_market_ac +'</span><div style="height: 15px;"></div>'+\
-        get_radio_button_Trader_prf() +\
-        '                   </div>'+\
-        '               <div style="height: 30px;"></div>'+\
-        '               <span class="text-info" style="width:200px;"><a href="'+ burl +'?cancel">'+ l_cancel_link +'</a></span><span>&nbsp;&nbsp;</span>'+\
-        '               <input type="submit" class="btn btn-info btn-lg active" style="max-width:800px; width: 100%" value="'+ l_save_btn +'">'+\
-        '               </form>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '   </div>'+\
-        '</div>'+\
-        '<div class="box">' +\
-        '   <div class="row">'+\
-        '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
-        '            <div class="box-part rounded" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
-        '               <span class="sectiont"><i class="fas fa-sliders-h"></i>&nbsp;'+ l_password +'</span><div style="height: 30px;"></div>'+\
-        '               <span class="text-primary" style="width:200px;">'+ l_password_label +'</span><span>&nbsp;&nbsp;</span>'+\
-        '               <a href="'+ burl +'reset/?password" class="btn btn-primary btn-md active" role="button" aria-pressed="true">'+ l_password_btn +'</a>'+\
-        '            </div>'+\
-        '        </div>'+\
-        '   </div>'+\
-        '</div>'
+            user_uid = user_get_uid()
+
+            connection = pymysql.connect(host=db_srv, user=db_usr, password=db_pwd, db=db_name, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+            cr = connection.cursor(pymysql.cursors.SSCursor)
+            sql = 'SELECT name, nickname, username, default_profile, lang FROM users WHERE uid="'+ str(user_uid) +'"'
+            cr.execute(sql)
+            rs = cr.fetchall()
+            name=''; nickname=''; username_email=''; default_profile='';language=''
+
+            for row in rs:
+                name = row[0]
+                nickname = row[1]
+                username_email = row[2]
+                default_profile = row[3]
+                language = row[4]
+
+            cr.close()
+            connection.close()
+
+            box_content = ' '+\
+            '<div class="box-top">' +\
+            '   <div class="row">'+\
+            '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
+            '            <div class="box-part rounded" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
+            '               <form name="settingsForm" id="settingsForm" method="post" action="'+ burl +'settings/?step=2" >'+\
+            '                   <span class="sectiont"><i class="fas fa-sliders-h"></i>&nbsp;'+ l_profile_section +'</span><div style="height: 30px;"></div>'+\
+            '                   <!--------------------- Name --------------------->'+\
+            '                   <div class="input-group" style="max-width:888px">'+\
+            '                       <span class="text-primary" style="width:200px;">'+ l_fullname +'</span><span>&nbsp;&nbsp;</span>'+\
+            '                       <div class="input-group-prepend">'+\
+            '                           <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-user-alt" style="font-size: large;"></i></span>'+\
+            '                       </div>'+\
+            '                           <input type="text" id="name" name="name" value="'+ str(name) +'" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_fullname +'" required autofocus>'+\
+            '                   </div>'+\
+            '                   <!--------------------- Nickname ----------------->'+\
+            '                   <div class="input-group" style="max-width:888px">'+\
+            '                       <span class="text-primary" style="width:200px;">'+ l_nickname +'</span><span>&nbsp;&nbsp;</span>'+\
+            '                       <div class="input-group-prepend">'+\
+            '                           <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-user-alt" style="font-size: large;"></i></span>'+\
+            '                       </div>'+\
+            '                           <input type="text" id="nickname" name="nickname" value="'+ str(nickname) +'" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_nickname +'" required autofocus>'+\
+            '                   </div>'+\
+            '                   <!--------------------- Email -------------------->'+\
+            '                   <div class="input-group" style="max-width:888px">'+\
+            '                       <span class="text-primary" style="width:200px;">'+ l_email +'</span><span>&nbsp;&nbsp;</span>'+\
+            '                       <div class="input-group-prepend">'+\
+            '                           <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-at" style="font-size: large;"></i></span>'+\
+            '                       </div>'+\
+            '                           <input type="email" id="email" name="email" value="'+ str(username_email) +'" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_email +'" required autofocus>'+\
+            '                   </div>'+\
+            '                   <div style="height: 30px;"></div>'+\
+            '                   <!---------------- Trader: Market ---------------->'+\
+            '                   <div>'+\
+            '                       <span class="text-primary">'+ l_market_ac +'</span><div style="height: 15px;"></div>'+\
+            get_radio_button_Trader_prf(default_profile) +\
+            '                   </div>'+\
+            '               <div style="height: 30px;"></div>'+\
+            '               <span class="text-info" style="width:200px;"><a href="'+ burl +'?cancel">'+ l_cancel_link +'</a></span><span>&nbsp;&nbsp;</span>'+\
+            '               <input type="submit" class="btn btn-info btn-lg active" style="max-width:800px; width: 100%" value="'+ l_save_btn +'">'+\
+            '               </form>'+\
+            '            </div>'+\
+            '        </div>'+\
+            '   </div>'+\
+            '</div>'+\
+            '<div class="box">' +\
+            '   <div class="row">'+\
+            '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
+            '            <div class="box-part rounded" style="'+ theme_return_this('','border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
+            '               <span class="sectiont"><i class="fas fa-sliders-h"></i>&nbsp;'+ l_password +'</span><div style="height: 30px;"></div>'+\
+            '               <span class="text-primary" style="width:200px;">'+ l_password_label +'</span><span>&nbsp;&nbsp;</span>'+\
+            '               <a href="'+ burl +'reset/?password" class="btn btn-primary btn-md active" role="button" aria-pressed="true">'+ l_password_btn +'</a>'+\
+            '            </div>'+\
+            '        </div>'+\
+            '   </div>'+\
+            '</div>'
 
     except Exception as e: print(e)
     return box_content
 
-def get_radio_button_Trader_prf():
+def get_radio_button_Trader_prf(default_profile):
     r = ''
     try:
         l_market = 'market'
         l_radioBtn_name = 'tradingProfile'
         value =''; label =''
+        checkedRadio = ''
+
         connection = pymysql.connect(host=db_srv, user=db_usr, password=db_pwd, db=db_name, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
         sql = 'SELECT market_id, market_label FROM markets ORDER BY market_label'
@@ -106,7 +128,9 @@ def get_radio_button_Trader_prf():
         for row in rs:
             value = row[0]
             label = row[1]
-            r = r + '<input type="radio" name="'+ l_radioBtn_name +'" value="'+ str(value) +'">&nbsp;'+ str(label) +'&nbsp;'+ l_market +'<br>'
+            checkedRadio =''
+            if value.lower() == default_profile.lower(): checkedRadio = 'checked'
+            r = r + '<input type="radio" name="'+ l_radioBtn_name +'" value="'+ str(value) +'" '+ checkedRadio +'>&nbsp;'+ str(label) +'&nbsp;'+ l_market +'<br>'
         print(sql)
         sql = 'SELECT asset_class_id, asset_class_name FROM asset_class  WHERE asset_class_id<>"'+ get_portf_suffix() +'" AND asset_class_id<>"MA:" ORDER BY asset_class_name'
         cr.execute(sql)
@@ -114,7 +138,9 @@ def get_radio_button_Trader_prf():
         for row in rs:
             value = row[0]
             label = row[1]
-            r = r + '<input type="radio" name="'+ l_radioBtn_name +'" value="'+ str(value) +'">&nbsp;'+ str(label)+'<br>'
+            checkedRadio =''
+            if value.lower() == default_profile.lower(): checkedRadio = 'checked'
+            r = r + '<input type="radio" name="'+ l_radioBtn_name +'" value="'+ str(value) +'" '+ checkedRadio +'>&nbsp;'+ str(label)+'<br>'
         cr.close()
         connection.close()
 
