9320c301f81479002beca0c7415f201e072768c0
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-26 11:52:33 +0700

    dev

--- app_search.py
+++ app_search.py
@@ -91,14 +91,8 @@ def get_box_search(burl,nonavbar):
         '       <div class="input-group-prepend"><span class="input-group-text" id="inputGroup-sizing-lg"><i class="fas fa-search" style="font-size: xx-large;"></i></span></div><input type="search" id="filterInput" name="'+ str(sid) +'" onkeyup="filterTable(); this.value = this.value.toUpperCase();" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_placeholder +'" autofocus></div><div>&nbsp;'+\
         '       </div>'+\
         '     <input type="hidden" name="sid" value="'+ str(sid) +'">'+\
-        '  </form>'+\
-        '  <script>'+\
-        '   window.onload = function() {'+\
-        '       alert("trigger");'+\
-        '       document.getElementById("filterInput").focus();'+\
-        '   }'+\
-        '  </script>'
-
+        '  </form>'
+        
         box_content = '' +\
         '<script>'+\
         'function filterTable() {'+\
