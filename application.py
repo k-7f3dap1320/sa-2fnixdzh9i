8103c61bb12cd45f2a13a33167f5144f8c7eeaf1
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-27 17:01:28 +0700

    dev

--- application.py
+++ application.py
@@ -111,12 +111,13 @@ def go():
         from_ip = request.values.get('from_ip')
         nickname = request.values.get('nickname')
         step = request.args.get('step')
+        broker = request.args.get('broker')
         mode = request.values.get('mode')
         if step == 'a': c = gen_createuser_avatar(appname,burl,err)
         elif step == 'b': c = save_avatar(burl,nickname)
         elif step == 'c': c = gen_selectmarket_page(appname,burl,mode)
         elif step == 'd': c= save_selectmarket(burl,mode,x)
-        else: c = gen_createuser_page(uid,appname,burl,name,username,password,from_ip)
+        else: c = gen_createuser_page(uid,appname,burl,name,username,password,from_ip,broker)
 
     elif request.endpoint == 'h':
         c = get_help_page(appname,burl)
--- createuser_main.py
+++ createuser_main.py
@@ -16,7 +16,6 @@ from awesomplete import *
 from tablesorter import *
 from app_navbar import *
 from font_awesome import *
-from createuser_form import *
 from sa_func import *
 import pymysql.cursors
 from sa_db import *
@@ -50,11 +49,11 @@ def set_nickname():
     return r
 
 
-def gen_createuser_page(uid,appname,burl,name,username,password,from_ip):
+def gen_createuser_page(uid,appname,burl,name,username,password,from_ip,broker):
     r = ''
     if uid == '0':
         r = get_head( get_loading_head() + get_googleanalytics() + get_title( appname ) + get_metatags(burl) + set_ogp(burl,1,'','') + get_bootstrap( get_sa_theme(),burl ) + get_tablesorter() + get_font_awesome() + get_stylesheet(burl) )
-        r = r + get_body( get_loading_body(), navbar(burl,0) + get_user_creation_form(burl) + get_page_footer(burl) )
+        r = r + get_body( get_loading_body(), navbar(burl,0) + get_user_creation_form(burl,broker) + get_page_footer(burl) )
         r = set_page(r)
     else:
         connection = pymysql.connect(host=db_srv, user=db_usr, password=db_pwd, db=db_name, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
@@ -82,3 +81,89 @@ def gen_createuser_page(uid,appname,burl,name,username,password,from_ip):
         cr.close()
         connection.close()
     return r
+
+def get_user_ip_input():
+    r = ''
+    try:
+        r = '' +\
+        '<script type="application/javascript">'+\
+        '  function getIP(json) {'+\
+        '    document.write("<input type=\'hidden\' value=\' ", json.ip,"\' id=\'from_ip\' name=\'from_ip\' >");'+\
+        '  }'+\
+        '</script>'+\
+        '<script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>'
+
+    except Exception as e:
+        print(e)
+    return r
+
+
+def get_user_creation_form(burl,broker):
+
+    box_content = ''
+
+    try:
+
+        l_enter_name = "Enter Name"
+        l_enter_email = "Enter Email"
+        l_received_payment_subscription_thank_you = "We have received your payment thank you."
+        l_note_user_creation_after_payment = "You are just 1 step away. Create your account."
+        box_content = '<div class="box-top">' +\
+        '   <div class="row">'+\
+        '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
+        '            <div class="box-part rounded">'+\
+        '     <form method="POST" action="'+ burl +'n/?uid='+ get_random_str(99)  +'" style="width: 100%; max-width: 600px; padding: 2%; margin: auto;">'+\
+        '         <div>'+\
+        '            <div>'+\
+        '<div style="text-align: center;"><span class="text-success"><h2>'+ l_received_payment_subscription_thank_you +'</h2></span></div>'+\
+        '<div style="text-align: center;">'+ l_note_user_creation_after_payment +'</div>'+\
+        '                <hr>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '        <div>'+\
+        '            <div>'+\
+        '               <div class="input-group input-group-lg">'+\
+        '                 <div class="input-group-prepend">'+\
+        '                   <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-user-alt" style="font-size: large;"></i></span>'+\
+        '                 </div>'+\
+        '                 <input type="text" id="name" name="name" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_enter_name +'" required autofocus>'+\
+        '               </div>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '        <div>'+\
+        '            <div>'+\
+        '               <div class="input-group input-group-lg">'+\
+        '                 <div class="input-group-prepend">'+\
+        '                   <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-at" style="font-size: large;"></i></span>'+\
+        '                 </div>'+\
+        '                 <input type="email" id="email" name="email" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="'+ l_enter_email +'" required autofocus>'+\
+        '               </div>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '        <div>'+\
+        '            <div>'+\
+        '               <div class="input-group input-group-lg">'+\
+        '                 <div class="input-group-prepend">'+\
+        '                   <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fa fa-key" style="font-size: large;"></i></span>'+\
+        '                 </div>'+\
+        '                 <input type="password" id="password" name="password" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Password" required>'+\
+        '               </div>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '        <div>'+\
+        '            <div>'+\
+        '                <div>&nbsp;</div>'+\
+        get_user_ip_input() +\
+        '                <button type="submit" class="btn btn-info btn-lg btn-block form-signin-btn">' + 'Next' + '&nbsp;<i class="fas fa-arrow-right"></i></button>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '<div class="expl" style="text-align: center;">*We respect your privacy and will never share your email address with any person or organization.</div>'+\
+        '    </form>'+\
+        '            </div>'+\
+        '        </div>'+\
+        '   </div>'+\
+        '</div>'
+
+    except Exception as e: print(e)
+
+    return box_content
