898ba6c8613905af906df9560bb387b558b306bf
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-30 12:17:55 +0700

    dev

--- intel_report.py
+++ intel_report.py
@@ -164,40 +164,41 @@ def get_expired_signals(burl):
 def get_signals_lines(burl):
     content = ''
     try:
-        l_title = 'Opportunities'
-        dn = datetime.datetime.now(); dnstr = dn.strftime("%Y%m%d");
-        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-        cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql ="SELECT DISTINCT "+\
-            "trades.uid "+\
-            "FROM trades "+\
-            "JOIN portfolios ON portfolios.symbol = trades.symbol "+\
-            "JOIN instruments ON instruments.symbol = portfolios.portf_symbol "+\
-            "JOIN instruments as a_alloc ON a_alloc.symbol = portfolios.symbol "+\
-            "WHERE "+\
-            "((portfolios.strategy_order_type = 'long' AND trades.order_type = 'buy') "+\
-            "OR (portfolios.strategy_order_type = 'short' AND trades.order_type = 'sell') "+\
-            "OR (portfolios.strategy_order_type = 'long/short') ) AND "+\
-            "(trades.entry_date >= " + dnstr + " AND instruments.owner = " + str(get_user_numeric_id()) + " AND status = 'active')"
-        cr.execute(sql)
-        rs = cr.fetchall()
-        content = ''+\
-        '<div class="row">' +\
-        '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"></div>'+\
-        '<div class="col-lg-10 col-md-10 col-sm-12 col-xs-12"><div class="box-part rounded"><h2>'+ l_title  +'</h2></div></div>'+\
-        '</div>'
-
-        for row in rs:
-            uid = row[0]
-            content = content +\
+        if user_is_login():
+            l_title = 'Opportunities'
+            dn = datetime.datetime.now(); dnstr = dn.strftime("%Y%m%d");
+            connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+            cr = connection.cursor(pymysql.cursors.SSCursor)
+            sql ="SELECT DISTINCT "+\
+                "trades.uid "+\
+                "FROM trades "+\
+                "JOIN portfolios ON portfolios.symbol = trades.symbol "+\
+                "JOIN instruments ON instruments.symbol = portfolios.portf_symbol "+\
+                "JOIN instruments as a_alloc ON a_alloc.symbol = portfolios.symbol "+\
+                "WHERE "+\
+                "((portfolios.strategy_order_type = 'long' AND trades.order_type = 'buy') "+\
+                "OR (portfolios.strategy_order_type = 'short' AND trades.order_type = 'sell') "+\
+                "OR (portfolios.strategy_order_type = 'long/short') ) AND "+\
+                "(trades.entry_date >= " + dnstr + " AND instruments.owner = " + str(get_user_numeric_id()) + " AND status = 'active')"
+            cr.execute(sql)
+            rs = cr.fetchall()
+            content = ''+\
             '<div class="row">' +\
             '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"></div>'+\
-            '    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><div class="box-part rounded">'+ get_tradingview_mini_chart(uid,'100%','200','false','1m',1) +'</div></div>'+\
-            '    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="box-part rounded">'+ get_signal_details(uid) + '<span style="font-size: small">'+ get_recomm(uid)  +'</span></div></div>'+\
-            '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"></div>'+\
+            '<div class="col-lg-10 col-md-10 col-sm-12 col-xs-12"><div class="box-part rounded"><h2>'+ l_title  +'</h2></div></div>'+\
             '</div>'
-        cr.close()
-        connection.close()
+
+            for row in rs:
+                uid = row[0]
+                content = content +\
+                '<div class="row">' +\
+                '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"></div>'+\
+                '    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><div class="box-part rounded">'+ get_tradingview_mini_chart(uid,'100%','200','false','1m',1) +'</div></div>'+\
+                '    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="box-part rounded">'+ get_signal_details(uid) + '<span style="font-size: small">'+ get_recomm(uid)  +'</span></div></div>'+\
+                '    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"></div>'+\
+                '</div>'
+            cr.close()
+            connection.close()
 
     except Exception as e: print(e)
     return content
