2cca665ac7b1036d76dfbaaac28cdc19f471672a
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-30 11:32:19 +0700

    dev

--- createuser_main.py
+++ createuser_main.py
@@ -135,9 +135,7 @@ def get_user_creation_form(burl,broker):
             payment_completed_header = ' '+\
             '<div style="text-align: center;"><span class="text-success"><h2>'+ l_received_payment_subscription_thank_you +'</h2></span></div>'+\
             '<div style="text-align: center;">'+ l_note_user_creation_after_payment +'</div>'
-        else:
-
-
+            
         box_content = '<div class="box-top">' +\
         '   <div class="row">'+\
         '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
