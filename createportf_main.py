33ac71484dc614000acb271ff6bcbf0e9b82bf52
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-10-22 16:05:26 +0700

    dev

--- createportf_main.py
+++ createportf_main.py
@@ -64,7 +64,7 @@ def get_selectportf_box(burl,step,mode,x):
         l_skip_process_button = 'done'
 
         button_back = '<span style="float:left;"><button type="button" style="font-size: medium;" class="btn btn-lg btn-secondary" onClick="javascript:history.back();"><i class="fas fa-caret-left"></i>&nbsp;'+ l_back_button +'</button></span>'
-        if step >1 and step < 6:
+        if step != '1' and step != '6':
             button_process_skip = '<span style="float:right;"><a href="'+ burl + 'p/?ins=3' +'" style="font-size: medium;" class="btn btn-lg btn-secondary">'+ l_skip_process_button +'&nbsp;<i class="fas fa-forward"></i></a></span>'
         else:
             button_process_skip = ''
