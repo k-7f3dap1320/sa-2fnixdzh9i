2aa1498d70fd3d2ed3644aac1e695c3571706d35
The Cat Tamer <dh@taatu.co>
2019-10-23 20:16:29 +0700

    dev

--- signinbox.py
+++ signinbox.py
@@ -27,9 +27,9 @@ def get_signin_box(burl):
             '                   <h1 style="text-align: left; font-size:x-large; font-weight:bolder;">'+ l_app_header_title +'</h1>   '+\
             '                   <div>'+ l_app_header_desc +'&nbsp;<a href="'+ burl +'pricing/" class="text-info">'+ l_app_call_to_action_link +'</a></div>'+\
             '                   <div class="row" style="margin: 20px;">'+\
-            '                       <img src="'+ burl +'static/etoro-logo.png" height="50px" style="margin:20px;" />'+\
-            '                       <img src="'+ burl +'static/tradingview-logo.png" height="50px" style="margin:20px;" />'+\
-            '                       <img src="'+ burl +'static/aws-logo.png" height="50px" style="margin:20px;" />'+\
+            '                       <a href="#" target="_blank"><img src="'+ burl +'static/etoro-logo.png" height="50px" style="margin:20px;" /></a>'+\
+            '                       <a href="#" target="_blank"><img src="'+ burl +'static/tradingview-logo.png" height="50px" style="margin:20px;" /></a>'+\
+            '                       <a href="#" target="_blank"><img src="'+ burl +'static/aws-logo.png" height="50px" style="margin:20px;" /></a>'+\
             '                   </div>'+\
             '                </div>'+\
             '                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="padding: 50px;">'+\
