43c36423789e2d2ba0a34d1b6a1916557e395f55
The Cat Tamer <dh@taatu.co>
2019-10-23 10:23:22 +0700

    dev

--- portf_gen_user_example.py
+++ portf_gen_user_example.py
@@ -26,7 +26,7 @@ def gen_portf_user_example(burl,acm,notstart):
         if user_is_login() == 1:
             connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
             cr = connection.cursor(pymysql.cursors.SSCursor)
-            sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.y1_signal > 0 AND instruments.m6_signal > 0 AND instruments.m3_signal > 0 ) AND (instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') ORDER BY RAND() LIMIT 5"
+            sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND symbol_list.disabled=0 AND (instruments.y1_signal > 0 AND instruments.m6_signal > 0 AND instruments.m3_signal > 0 ) AND (instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') ORDER BY RAND() LIMIT 5"
             cr.execute(sql)
             rs = cr.fetchall()
             i = 1
