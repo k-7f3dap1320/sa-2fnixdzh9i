248a4e1af5877ce3d0835d352aed49356c84150e
The Cat Tamer <dh@taatu.co>
2019-10-23 20:34:26 +0700

    dev

--- signinbox.py
+++ signinbox.py
@@ -5,6 +5,7 @@
 from sa_func import *
 from signin_main import *
 from app_cookie import *
+from createuser_main import *
 
 def get_signin_box(burl):
 
@@ -17,8 +18,11 @@ def get_signin_box(burl):
             l_app_header_title = 'Create unlimited optimized trading portfolios'
             l_app_header_desc = 'Chart patterns, price movements, and news analysed using quantitative methods with the power of artificial intelligence to generate trading signals. SmartAlpha is your trading assistant to generate more profitable trades.'
             l_app_call_to_action_link = 'Join now.'
+            etoro_logo_form = broker_link_form = go_to_url(get_broker_affiliate_link('eToro'),'form','eToro')
+            etoro_logo_link = broker_link_form = go_to_url(get_broker_affiliate_link('eToro'),'link','eToro')
 
-            box_content = '<div class="box-sign"><div class="row">' +\
+            box_content = etoro_logo_form +\
+            '<div class="box-sign"><div class="row">' +\
             '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
             '            <div class="sign-part">'+\
             '               <div class="row sign-row">'+\
@@ -27,7 +31,7 @@ def get_signin_box(burl):
             '                   <h1 style="text-align: left; font-size:x-large; font-weight:bolder;">'+ l_app_header_title +'</h1>   '+\
             '                   <div>'+ l_app_header_desc +'&nbsp;<a href="'+ burl +'pricing/" class="text-info">'+ l_app_call_to_action_link +'</a></div>'+\
             '                   <div class="row" style="margin: 20px;">'+\
-            '                       <a href="#" target="_blank"><img src="'+ burl +'static/etoro-logo.png" height="50px" style="margin:20px;" /></a>'+\
+            '                       <a href="'+ etoro_logo_link +'" target="_blank"><img src="'+ burl +'static/etoro-logo.png" height="50px" style="margin:20px;" /></a>'+\
             '                       <a href="#" target="_blank"><img src="'+ burl +'static/tradingview-logo.png" height="50px" style="margin:20px;" /></a>'+\
             '                       <a href="#" target="_blank"><img src="'+ burl +'static/aws-logo.png" height="50px" style="margin:20px;" /></a>'+\
             '                   </div>'+\
