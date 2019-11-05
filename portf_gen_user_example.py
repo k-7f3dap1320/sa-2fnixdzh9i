cfff887a999e9bb879f9b244edb82f9307945d40
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-11-05 14:57:13 +0700

    dev

--- portf_gen_user_example.py
+++ portf_gen_user_example.py
@@ -18,21 +18,32 @@ db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access
 def gen_portf_user_example(burl,acm,notstart):
 
     resp = make_response( redirect(burl+'genportf?step=2&notstart='+str(notstart)) )
-    try:
-        if acm is None:
-            asset_class = '%%'
-        else:
-            asset_class = acm
-        volatilty_risk_fx = 0.01
-        if asset_class == "FX:":
-            volatility_risk = volatility_risk_fx
-        else:
-            volatility_risk = 0.05
-        
-        if user_is_login() == 1:
-            connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-            cr = connection.cursor(pymysql.cursors.SSCursor)
-            sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND symbol_list.disabled=0 AND (instruments.y1_signal > 0 AND instruments.volatility_risk_st < "+ str(volatility_risk) +" ) AND (instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') ORDER BY RAND() LIMIT 5"
+    if acm is None:
+        asset_class = '%%'
+    else:
+        asset_class = acm
+    volatility_risk_fx = 0.01
+    if asset_class == "FX:":
+        volatility_risk = volatility_risk_fx
+    else:
+        volatility_risk = 0.05
+    
+    if user_is_login() == 1:
+        connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND symbol_list.disabled=0 AND (instruments.y1_signal > 0 AND instruments.volatility_risk_st < "+ str(volatility_risk) +" ) AND (instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') ORDER BY RAND() LIMIT 5"
+        cr.execute(sql)
+        rs = cr.fetchall()
+        i = 0
+        for row in rs:
+            resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+            resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+            resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+            i += 1
+        if i < 5:
+            sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol "+\
+            "WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.m6_signal > 0 AND instruments.volatility_risk_st < "+ str(volatility_risk) +") AND "+\
+            "(instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') AND symbol_list.disabled = 0 ORDER BY RAND() LIMIT 5"
             cr.execute(sql)
             rs = cr.fetchall()
             i = 0
@@ -41,33 +52,18 @@ def gen_portf_user_example(burl,acm,notstart):
                 resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
                 resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
                 i += 1
-            if i < 5:
-                sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol "+\
-                "WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.m6_signal > 0 AND instruments.volatility_risk_st < "+ str(volatility_risk) +") AND "+\
-                "(instruments.asset_class LIKE '"+ asset_class +"' OR instruments.market LIKE '"+ asset_class +"') AND symbol_list.disabled = 0 ORDER BY RAND() LIMIT 5"
-                cr.execute(sql)
-                rs = cr.fetchall()
-                i = 0
-                for row in rs:
-                    resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    i += 1
-            if i < 5:
-                add_additional_asset = "FX:"
-                sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol "+\
-                "WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.y1_signal > 0 AND instruments.volatility_risk_st < "+ str(volatility_risk_fx) +" ) AND "+\
-                "(instruments.asset_class LIKE '"+ add_additional_asset +"') AND symbol_list.disabled = 0 ORDER BY RAND() LIMIT 5"
-                cr.execute(sql)
-                rs = cr.fetchall()
-                for row in rs:
-                    resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
-                    i += 1
-
-
-    except Exception as e: print(e)
+        if i < 5:
+            add_additional_asset = "FX:"
+            sql = "SELECT symbol_list.uid FROM instruments JOIN symbol_list ON symbol_list.symbol = instruments.symbol "+\
+            "WHERE instruments.symbol NOT LIKE '%"+ get_portf_suffix() +"%' AND (instruments.y1_signal > 0 AND instruments.volatility_risk_st < "+ str(volatility_risk_fx) +" ) AND "+\
+            "(instruments.asset_class LIKE '"+ add_additional_asset +"') AND symbol_list.disabled = 0 ORDER BY RAND() LIMIT 5"
+            cr.execute(sql)
+            rs = cr.fetchall()
+            for row in rs:
+                resp.set_cookie('portf_s_'+str(i), str(row[0]), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                resp.set_cookie('portf_s_'+str(i)+'_conv', str('weak'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                resp.set_cookie('portf_s_'+str(i)+'_type', str('long/short'), expires=datetime.datetime.now() + datetime.timedelta(days=1) )
+                i += 1
     return resp
 
 def gen_portf_validate_content(burl,notstart):
