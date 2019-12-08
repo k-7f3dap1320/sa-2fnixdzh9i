a935b008c959db071a8ea4d96a72da280c8021ec
The Cat Tamer <dh@taatu.co>
2019-12-08 15:28:55 +0700

    dev

--- sa_terminal.py
+++ sa_terminal.py
@@ -24,8 +24,8 @@ def get_terminal_button_func(burl, func_name):
     '<script>'+\
     'function '+ func_name +'{'+\
     'if((screen.availHeight || screen.height-30) > window.innerHeight) {'+\
-    'var screen_y = window.screen.height/1.01;'+\
-    'var screen_x = window.screen.width/1.01;'+\
+    'var screen_y = window.screen.height/1.1;'+\
+    'var screen_x = window.screen.width/1.1;'+\
     'var screen_y_half = screen_y/2;'+\
     'var screen_x_half = screen_x/2;'+\
     'var screen_x_quart = screen_x/4;'+\
