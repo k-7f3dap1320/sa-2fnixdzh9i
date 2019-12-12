38cd0179e766a3226cea490f2de4c6e5525ce206
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-12-12 16:13:55 +0700

    dev

--- sa_terminal.py
+++ sa_terminal.py
@@ -69,7 +69,7 @@ def get_terminal_button_func(burl, func_name):
     'var screen_x_right = screen_x_half + screen_x_quart;'+\
     'var common_args = \'location=no, menubar=no, status=no,toolbar=no\';'+\
     'var help_panel_args = '+\
-    '\'width=300, height=\'+ screen_y_half +\', left=0, top=\'+ screen_x_half +\',\'+ common_args;'+\
+    '\'width=500, height=\'+ screen_y_half +\', left=0, top=\'+ screen_x_half +\',\'+ common_args;'+\
     open_window_args(burl+'terminalhelp/', 'help_panel_args')+\
     'var newsfeed_args = '+\
     '\'width=\'+ screen_x_half +\', height=\'+ screen_y +\', left=0, top=0,\'+ common_args;'+\
