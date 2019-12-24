8d60fc2e52021a4b5b00cfea9b221bb1ddb7569b
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-12-24 15:58:59 +0700

    dev

--- application.py
+++ application.py
@@ -290,4 +290,4 @@ if __name__ == '__main__':
     if not DEV_MODE:
         application.run()
     else:
-        application.run(host='0.0.0.0', port=80, threaded=True)
+        application.run(host='0.0.0.0', port=80, debug=DEV_MODE, threaded=True)
