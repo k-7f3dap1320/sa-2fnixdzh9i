b42e41cd6fbaae1e30508f5d42994320a2f429b0
The Cat Tamer <dh@taatu.co>
2019-12-25 19:07:01 +0700

    dev

--- document.py
+++ document.py
@@ -105,12 +105,13 @@ def get_doc_page(appname, burl, uid, terminal):
     document = doc_data(uid)
     title = document.get_title()
     content = document.get_content()
+    page_url = burl+'doc/?uid=' + str(uid)
 
     return_data = get_head(get_loading_head() +\
                            get_googleanalytics() +\
                            get_title(appname) +\
                            get_metatags(burl) +\
-                           set_ogp(burl, 2, title, '') +\
+                           set_ogp(page_url, 2, title, content[0:100]) +\
                            get_bootstrap(get_sa_theme(), burl) +\
                            get_font_awesome() +\
                            get_stylesheet(burl))
