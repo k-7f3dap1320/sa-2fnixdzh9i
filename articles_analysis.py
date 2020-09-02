2d6e0e79cf6d4daddac8f205317a78d4ec9310f7
The Cat Tamer <dh@taatu.co>
2020-09-02 15:34:14 +0700

    dev

--- articles_analysis.py
+++ articles_analysis.py
@@ -1,5 +1,7 @@
 """ Articles and Analysis section """
 import pymysql.cursors
+from app_cookie import theme_return_this
+from sa_func import get_elapsed_time
 
 from sa_db import sa_db_access
 ACCESS_OBJ = sa_db_access()
@@ -26,17 +28,21 @@ def get_list_articles(burl, max_list):
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
     cursor = connection.cursor(pymysql.cursors.SSCursor)
-    sql = 'SELECT uid, title FROM documents WHERE category LIKE "%'+ str(doc_cat) +'%" '+\
+    sql = 'SELECT uid, title, date FROM documents WHERE category LIKE "%'+ str(doc_cat) +'%" '+\
     'ORDER BY date DESC LIMIT ' + str(max_list)
     cursor.execute(sql)
     res = cursor.fetchall()
     for row in res:
         uid = row[0]
         title = row[1]
+        article_date = get_elapsed_time(row[2])
         ret = ret +\
         '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" '+\
         'style="border-top:0.5px; border-top-style: dotted; text-align: left;">'+\
         '<i class="fas fa-file-alt"></i> '+\
+        '&nbsp;<span style="'+\
+        theme_return_this('color:black;', 'color:#00ffff;') +' ">'+\
+        str(article_date) +'</span>&nbsp;'+\
         '<a href="'+ str(burl) +'doc/?uid='+ str(uid) +'" target="_blank">'+ str(title) +'</a>'+\
         '</div>'
     cursor.close()
