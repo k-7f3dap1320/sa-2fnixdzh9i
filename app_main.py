1c460307be6923d9c42505fab12aab12aa4e3c7c
The Cat Tamer <dh@taatu.co>
2020-09-02 11:08:36 +0700

    dev

--- app_main.py
+++ app_main.py
@@ -73,13 +73,13 @@ def get_page_content(burl, terminal, selection):
     news_feed_content = get_feed(burl, terminal, selection)
     tab_content = ''+\
     '<ul class="nav nav-tabs">'+\
-    '<li class="active"><a class="nav-link" data-toggle="pill" href="#signals">Signals</a></li>'+\
+    '\<a class="nav-link active" data-toggle="pill" href="#signals">Signals</a>'+\
     '<li><a  class="nav-link" data-toggle="pill" href="#feed">Feed</a></li>'+\
     '<li><a  class="nav-link" data-toggle="pill" href="#analysis">Analysis</a></li>'+\
     '</ul>'+\
     ''+\
     '<div class="tab-content">'+\
-    '<div id="signals" class="tab-pane fade in active">'+\
+    '<div id="signals" class="tab-pane fade active">'+\
     '</div>'+\
     '<div id="feed" class="tab-pane fade">'+\
     news_feed_content +\
