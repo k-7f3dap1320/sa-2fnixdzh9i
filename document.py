3bd3f6cde14f37b8cf57cc290efd15df22bdf4f8
The Cat Tamer <dh@taatu.co>
2020-09-04 14:39:34 +0700

    dev

--- document.py
+++ document.py
@@ -16,6 +16,7 @@ from googleanalytics import get_googleanalytics
 from app_stylesheet import get_stylesheet
 from app_cookie import get_sa_theme
 from print_google_ads import print_google_ads
+from document_menu import get_doc_menu
 from sa_db import sa_db_access
 ACCESS_OBJ = sa_db_access()
 DB_USR = ACCESS_OBJ.username()
@@ -27,6 +28,7 @@ class doc_data:
     """ xxx """
     title = ''
     content = ''
+    category = ''
 
     def __init__(self, uid):
         connection = pymysql.connect(host=DB_SRV,
@@ -36,18 +38,22 @@ class doc_data:
                                      charset='utf8mb4',
                                      cursorclass=pymysql.cursors.DictCursor)
         cursor = connection.cursor(pymysql.cursors.SSCursor)
-        sql = 'SELECT title, content FROM documents WHERE uid='+ str(uid)
+        sql = 'SELECT title, content, category FROM documents WHERE uid='+ str(uid)
         cursor.execute(sql)
         res = cursor.fetchall()
         for row in res:
             self.title = row[0]
             self.content = row[1]
+            self.category = row[2]
 
     def get_title(self):
         return self.title
 
     def get_content(self):
         return self.content
+    
+    def get_category(self):
+        return self.category
 
 def get_doc_illustration(burl):
     ret = ''
@@ -56,7 +62,7 @@ def get_doc_illustration(burl):
     ret = '<img src="'+ str(file_location) +'" width="100%" alt="article image">'
     return ret
 
-def get_doc_content(title, content, terminal):
+def get_doc_content(burl, title, content, category, terminal):
     """ Content of the page """
     content_height = ''
     iframe_margin = ''
@@ -80,6 +86,7 @@ def get_doc_content(title, content, terminal):
     '        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+\
     '            <div class="box-part rounded" '+\
     '            style="height:'+ toolbar_height +'; padding:'+ toolbar_padding +';">'+\
+    get_doc_menu(burl, category)+\
     '            </div>'+\
     '            <div class="box-part rounded" style="height:'+ content_height +';'+\
     '            overflow: hidden;">'+\
@@ -101,6 +108,7 @@ def get_doc_page(appname, burl, uid, terminal):
     document = doc_data(uid)
     title = document.get_title()
     content = document.get_content()
+    category = document.get_category()
     page_url = burl+'doc/?uid=' + str(uid)
 
     return_data = get_head(get_loading_head() +\
@@ -113,7 +121,7 @@ def get_doc_page(appname, burl, uid, terminal):
                            get_stylesheet(burl))
     return_data = return_data +\
     get_body(get_loading_body(), navbar(burl, 0, terminal) +\
-             get_doc_content(title, content, terminal) +\
+             get_doc_content(burl, title, content, category, terminal) +\
              get_page_footer(burl, False),'overflow: hidden;')
     return_data = set_page(return_data)
     return return_data
---
+++ document_menu.py
@@ -0,0 +1,82 @@
+""" document toolbar, top menu """
+import pymysql.cursors
+from sa_db import sa_db_access
+ACCESS_OBJ = sa_db_access()
+DB_USR = ACCESS_OBJ.username()
+DB_PWD = ACCESS_OBJ.password()
+DB_NAME = ACCESS_OBJ.db_name()
+DB_SRV = ACCESS_OBJ.db_server()
+
+def get_doc_menu(burl, category):
+    """
+    Return default menu unless:
+    category is: 
+        [...]
+    """
+    ret = default_menu()
+    return ret
+
+def default_menu(burl):
+    """ xxx """
+    ret = ''
+    sql = ''
+    menu_label = ''
+    color = ''
+    # Top Analysis (color: primary)
+    sql = "SELECT uid, title FROM documents WHERE "+\
+    "category LIKE %perma% ORDER BY title"
+    color = "primary"
+    menu_label = "Top Analysis"
+    ret = ret + default_menu_content(sql, color, burl, menu_label)
+    
+    # By Asset Class (color: info)
+    sql = "SELECT uid, title FROM documents WHERE "+\
+    "category LIKE %assetclass% ORDER BY title"
+    color = "info"
+    menu_label = "By Asset Class"
+    ret = ret + default_menu_content(sql, color, burl, menu_label)
+    
+    # By Sectors (color: warning)
+    sql = "SELECT uid, title FROM documents WHERE "+\
+    "category LIKE %sector% ORDER BY title"
+    color = "warning"
+    menu_label = "By Sectors"
+    ret = ret + default_menu_content(sql, color, burl, menu_label)
+    
+    # Latest Analysis (color: danger)
+    sql = "SELECT uid, title FROM documents WHERE "+\
+    "category LIKE %article% ORDER BY date DESC"
+    color = "danger"
+    menu_label = "Latest"
+    ret = ret + default_menu_content(sql, color, burl, menu_label)
+    
+    return ret
+
+def default_menu_content(sql, color, burl, menu_label):
+    ret = ''
+
+    connection = pymysql.connect(host=DB_SRV,
+                                 user=DB_USR,
+                                 password=DB_PWD,
+                                 db=DB_NAME,
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor(pymysql.cursors.SSCursor)
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    ret = ''+\
+    '<div class="btn-group">'+\
+    '  <button type="button" class="btn btn-'+ str(color) +' dropdown-toggle" '+\
+    'data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+\
+    menu_label+\
+    '  </button>'+\
+    '  <div class="dropdown-menu">'
+
+    for row in res:
+        uid = row[0]
+        title = row[1]
+        '<a class="dropdown-item" href="'+ burl + 'doc/?uid='+ str(uid) +'">'+ str(title) +'</a>'
+    ret = ret +\
+    '  </div>'+\
+    '</div>'
+    return ret
