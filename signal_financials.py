56f38a09094f40d154dee2ed0d433cbfcc1431b5
The Cat Tamer <dh@taatu.co>
2020-11-05 09:58:59 +0700

    dev

--- signal_financials.py
+++ signal_financials.py
@@ -78,8 +78,6 @@ def get_financials_content(uid, burl, terminal):
     '*** (1) URL is '+ str(burl) +\
     '            </div>'+\
     '        </div>'+\
-    '   </div>'+\
-    '   <div class="row">'+\
     '        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">'+\
     '            <div class="box-part rounded sa-center-content" style="'+\
     theme_return_this('', 'border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
