7fd10c7550650ad4d15fe731ea021e031c92538b
The Cat Tamer <dh@taatu.co>
2020-11-05 09:54:29 +0700

    dev

--- signal_financials.py
+++ signal_financials.py
@@ -67,9 +67,18 @@ def get_financials_content(uid, burl, terminal):
     '        </div>'+\
     '   </div>'+\
     '</div>'
-
+    """ Content start here """
+    """"""""""""""""""""""""""
     box_content = box_content +\
    '<div class="box">' +\
+    '   <div class="row">'+\
+    '        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">'+\
+    '            <div class="box-part rounded sa-center-content" style="'+\
+    theme_return_this('', 'border-style:solid; border-width:thin; border-color:#343a40;') +'">'+\
+    '*** URL is '+ str(burl) +\
+    '            </div>'+\
+    '        </div>'+\
+    '   </div>'+\
     '   <div class="row">'+\
     '        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">'+\
     '            <div class="box-part rounded sa-center-content" style="'+\
@@ -83,6 +92,7 @@ def get_financials_content(uid, burl, terminal):
     '*** URL is '+ str(burl) +\
     '            </div>'+\
     '        </div>'+\
+
     '   </div>'+\
     '</div>'
 
